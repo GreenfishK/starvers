a68bcd5e4202ebb775e9168780d0da7e2e68459f
Ruben Taelman <ruben.taelman@ugent.be>
2017-07-03 09:29:04 +0200

    Finalize Docker run script

--- src/jena_TDB/run-docker.sh
+++ src/jena_TDB/run-docker.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+policies="cbtb"
+categories="mat diff ver"
+queries="s-queries-lowCardinality.txt s-queries-highCardinality.txt p-queries-highCardinality.txt p-queries-lowCardinality.txt o-queries-highCardinality.txt o-queries-lowCardinality.txt po-queries-highCardinality.txt po-queries-lowCardinality.txt so-queries-lowCardinality.txt sp-queries-highCardinality.txt sp-queries-lowCardinality.txt spo-queries.txt"
+
+# TODO
+#categories="mat diff ver"
+#queries="po-queries-highCardinality.txt po-queries-lowCardinality.txt so-queries-lowCardinality.txt sp-queries-highCardinality.txt sp-queries-lowCardinality.txt spo-queries.txt"
+
+for policy in ${policies[@]}; do
+for category in ${categories[@]}; do
+for query in ${queries[@]}; do
+
 docker run -it --rm \
-    -e POLICY="ic" \
-    -e ROLE="s" \
-    -e CATEGORY="mat" \
-    -e QUERY="subjectLookup/queries-sel-10-e0.2.txt" \
+    -e POLICY="$policy" \
+    -e ROLE="SPO" \
+    -e CATEGORY="$category" \
+    -e QUERY="$query" \
     -v /mnt/datastore/data/dslab/experimental/patch/tdb/:/var/data/dataset/ \
-    -v /mnt/datastore/data/dslab/experimental/patch/BEAR/queries/:/var/data/queries/ \
+    -v /mnt/datastore/data/dslab/experimental/patch/BEAR/queries_new/:/var/data/queries/ \
     -v /mnt/datastore/data/dslab/experimental/patch/output/:/var/data/output/ \
     bear-jena
+
+done
+done
+done
