a71d39c8e2eaa0b43a9ec6640c0f19aa07ba2838
Filip Kovacevic <f.kovacevic@gmx.at>
2021-12-08 23:04:04 +0100

    added python script to load the output csv files and plot the query time for each version as line chart

--- scripts/visualization/plot.py
+++ scripts/visualization/plot.py
@@ -1,22 +1,39 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
-import datetime
+from datetime import datetime
+from datetime import timezone
+import socket
+
+hostname = socket.gethostname()
 
 
-def plot_mat_simple(timestamp: datetime):
+def plot_mat_lookup_queries(timestamp: datetime, policy: str, triple_pattern: str):
     """
     Version materialisation queries with simple lookups for IC, CB and TB approaches.
+    :param triple_pattern: s, p, o, sp, po, so, spo
+    :param policy: ic, tb or cb
     :param timestamp:
     :return:
     """
-    df = pd.read_csv
 
+    ts_formatted = timestamp.strftime("%Y-%m-%dT%H:%M:%S")
+    output_dir = "~/.BEAR/output/time/bearb-hour/{hostname}-{timestamp}".format(hostname=hostname,
+                                                                                timestamp=ts_formatted)
+    filename = "time-{policy}-mat-lookup_queries_{triple_pattern}.csv".format(policy=policy,
+                                                                              triple_pattern=triple_pattern)
+    df = pd.read_csv("{dir}/{fn}".format(dir=output_dir, fn=filename), delimiter=" ", skiprows=1,
+                     names=['ver', 'min', 'mean', 'max', 'stddev', 'cnt_queries', '?'])
 
-def plot_mat_other(timestamp: datetime):
-    """
-    Version materialisation queries with other triple patterns for IC, CB and TB approaches.
-    :param timestamp:
-    :return:
-    """
-    df = pd.read_csv
+    ax = plt.gca()
+    df.plot(kind="line", x="ver", y="mean", ax=ax)
+    ax.set_xlabel("Version")
+    ax.set_ylabel("query time in ms")
+
+    plt.show()
+
+# simple lookups, 49 queries
+plot_mat_lookup_queries(datetime(2021, 12, 8, 22, 11, 19), "tb", "p")
+
+# other triple patterns, 13 queries
+plot_mat_lookup_queries(datetime(2021, 12, 8, 22, 11, 19), "tb", "po")
