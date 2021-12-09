23c3f981f5c128309929eefba488d74e4d8581e3
Filip Kovacevic <f.kovacevic@gmx.at>
2021-12-09 19:47:28 +0100

    added some TODOs and a shell script to load the tb_star dataset into jena. This has not been tested yet. Refactored scripts directory

---
+++ data/load-bearb-hourly-tb_star-jena.sh
@@ -0,0 +1,17 @@
+bin/bash
+sudo rm -rf $(pwd)/tdb-bearb-hour/tb_star/* # to clear database files created by jena if the script needs to be re-executed
+
+time docker run \
+    -it \
+    --rm \
+    -v $(pwd)/tdb-bearb-hour/:/var/data/out/ \
+    -v $(pwd)/rawdata-bearb/hour/:/var/data/in/ \
+    stain/jena /jena/bin/tdbloader2 \
+        --loc /var/data/out/tb /var/data/in/alldata.TB_star.nts \
+    > output/load-bearb-hour-tb_star--.txt
+
+# stain/jena --sort-args "-S=16G" \ # returned an error message with the latest jena/stain image as of 04.12.2021
+#docker run -it --rm \
+#    -v $(pwd)/tdb/:/var/data/in/ \
+#    -v $(pwd)/rawdata/alldata.IC.nq/:/var/data/out/ \
+#    stain/jena /jena/bin/tdbloader2 --help
---
+++ src/jena_TDB_star/README.md
@@ -0,0 +1,1 @@
+TODO: Write script that runs the two sets of simple lookup queries and computes the execution time (and other descriptive statistics) for each version of the Jena TB RDF* approach.
