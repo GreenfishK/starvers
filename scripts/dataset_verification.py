445d0f1165bc43ae96cad9b2069ca59e646415bd
Filip Kovacevic <f.kovacevic@gmx.at>
2021-12-11 19:36:25 +0100

    first method written: number of triples for version x

--- scripts/dataset_verification.py
+++ scripts/dataset_verification.py
@@ -5,12 +5,18 @@ from pathlib import Path
 ic0_ds_path = str(Path.home()) + "/.BEAR/rawdata-bearb/hour/alldata.IC.nt/000001.nt"
 ic1_ds_path = str(Path.home()) + "/.BEAR/rawdata-bearb/hour/alldata.IC.nt/000002.nt"
 
-ic0_df = pd.DataFrame({'s': [], 'p': [], 'o': []})
-ic0_list = []
-with open(ic0_ds_path, "r") as ic0:
-    for triple in ic0:
-        tr_array = triple[:-2].split(" ", 2)
-        ic0_list.append(tr_array)
 
-ic0_df = pd.DataFrame(ic0_list, columns=['s', 'p', 'o'])
-print(ic0_df)
+def number_of_triples(version: int):
+    ic0_ds_path = str(Path.home()) + "/.BEAR/rawdata-bearb/hour/alldata.IC.nt/00{0}.nt".format(str(version).zfill(4))
+    ic0_list = []
+    with open(ic0_ds_path, "r") as ic0:
+        for triple in ic0:
+            tr_array = triple[:-2].split(" ", 2)
+            if len(tr_array) == 3:
+                ic0_list.append(tr_array)
+
+    ic0_df = pd.DataFrame(ic0_list, columns=['s', 'p', 'o'])
+    print(len(ic0_df))
+
+
+number_of_triples(1299)
