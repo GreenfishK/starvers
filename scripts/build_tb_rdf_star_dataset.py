93d580e4550459905a40d9fc78408d7cb51c7f61
Filip Kovacevic <f.kovacevic@gmx.at>
2021-12-24 12:41:40 +0100

    changed all the functions now to use rdflib and graph processing instead of dataframes

--- scripts/build_tb_rdf_star_dataset.py
+++ scripts/build_tb_rdf_star_dataset.py
@@ -12,52 +12,45 @@ pd.set_option('display.width', desired_width)
 pd.set_option('display.max_columns', 10)
 
 
-def diff_set(version1: int, version2: int):
+def diff_set(version1: int, version2: int) -> [Graph, Graph]:
     ic1_ds_path = str(Path.home()) + "/.BEAR/rawdata-bearb/hour/alldata.IC.nt/00{0}.nt".format(str(version1).zfill(4))
     ic2_ds_path = str(Path.home()) + "/.BEAR/rawdata-bearb/hour/alldata.IC.nt/00{0}.nt".format(str(version2).zfill(4))
 
     ic1 = Graph()
     ic1.parse(ic1_ds_path)
-    ic1_list = []
-    for s, p, o in ic1:
-        ic1_list.append([s.n3(), p.n3(), o.n3()])
-    ic1_df = pd.DataFrame(ic1_list, columns=['s1', 'p1', 'o1'])
-
     ic2 = Graph()
     ic2.parse(ic2_ds_path)
-    ic2_list = []
-    for s, p, o in ic2:
-        ic2_list.append([s.n3(), p.n3(), o.n3()])
-    ic2_df = pd.DataFrame(ic2_list, columns=['s1', 'p1', 'o1'])
 
-    df_del = ic1_df.merge(ic2_df, indicator='i', how='left').query('i == "left_only"').drop('i', 1)
-    df_del['change_type'] = 'd'
-    df_add = ic1_df.merge(ic2_df, indicator='i', how='right').query('i == "right_only"').drop('i', 1)
-    df_add['change_type'] = 'a'
-    df = pd.concat([df_del, df_add])
-    df['dot'] = "."
-    return df
+    cs_add = Graph()
+    cs_add.parse(ic2_ds_path)
+    cs_add.__isub__(ic1)
+
+    cs_del = Graph()
+    cs_del.parse(ic1_ds_path)
+    cs_del.__isub__(ic2)
+
+    return cs_add, cs_del
 
 
-def construct_change_sets():
+def construct_change_sets(start_vers: int, end_vers: int):
     cb_comp_dir = str(Path.home()) + "/.BEAR/rawdata-bearb/hour/alldata.CB_computed.nt"
     if not os.path.exists(cb_comp_dir):
         os.makedirs(cb_comp_dir)
 
-    for i in range(1, 1299):
+    for i in range(start_vers, end_vers):
         output = diff_set(i, i + 1)
-        cs_added = output.query("change_type == 'a'")
-        assert isinstance(cs_added, pd.DataFrame)
-        cs_deleted = output.query("change_type == 'd'")
-        assert isinstance(cs_deleted, pd.DataFrame)
+        cs_added = output[0]
+        assert isinstance(cs_added, Graph)
+        cs_deleted = output[1]
+        assert isinstance(cs_deleted, Graph)
 
         print("Create and load data-added_{0}-{1}.nt".format(i, i + 1))
         f = open(cb_comp_dir + "/" + "data-added_{0}-{1}.nt".format(i, i + 1), "w")
         f.write("")
         f.close()
         with open(cb_comp_dir + "/" + "data-added_{0}-{1}.nt".format(i, i + 1), "a") as output_tb_ds:
-            for index, row in cs_added.iterrows():
-                output_tb_ds.write("{0} {1} {2} .\n".format(row['s1'], row['p1'], row['o1']))
+            for s, p, o in cs_added:
+                output_tb_ds.write("{0} {1} {2} .\n".format(s.n3(), p.n3(), o.n3()))
             output_tb_ds.close()
 
         print("Create and load data-deleted_{0}-{1}.nt".format(i, i + 1))
@@ -65,8 +58,8 @@ def construct_change_sets():
         f.write("")
         f.close()
         with open(cb_comp_dir + "/" + "data-deleted_{0}-{1}.nt".format(i, i + 1), "a") as output_tb_ds:
-            for index, row in cs_deleted.iterrows():
-                output_tb_ds.write("{0} {1} {2} .\n".format(row['s1'], row['p1'], row['o1']))
+            for s, p, o in cs_deleted:
+                output_tb_ds.write("{0} {1} {2} .\n".format(s.n3(), p.n3(), o.n3()))
             output_tb_ds.close()
 
 
@@ -164,5 +157,5 @@ def construct_tb_star_ds(cb_rel_path: str):
 
 
 # Take the change sets that were manually computed from the ICs by compute_change_sets.py
-construct_change_sets()
-construct_tb_star_ds("alldata.CB_computed.nt")
+construct_change_sets(7, 8)
+# construct_tb_star_ds("alldata.CB_computed.nt")
