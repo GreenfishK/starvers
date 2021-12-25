4957c73190adb81358601daac0b560aba9c588ab
Filip Kovacevic <f.kovacevic@gmx.at>
2021-12-25 21:19:21 +0100

    updated bash scripts to load data

--- data/load-bearb-hourly-tb_star-jena.sh
+++ data/load-bearb-hourly-tb_star-jena.sh
@@ -7,7 +7,7 @@ time docker run \
     -v $(pwd)/tdb-bearb-hour/:/var/data/out/ \
     -v $(pwd)/rawdata-bearb/hour/:/var/data/in/ \
     stain/jena /jena/bin/tdbloader2 \
-        --loc /var/data/out/tb /var/data/in/alldata.TB_star.ttl \
+        --loc /var/data/out/tb_star /var/data/in/alldata.TB_star.ttl \
     > output/load-bearb-hour-tb_star--.txt
 
 # stain/jena --sort-args "-S=16G" \ # returned an error message with the latest jena/stain image as of 04.12.2021
