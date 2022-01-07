28727d8369fa7d1fae950a94ef898ac38ad77349
Filip Kovacevic <f.kovacevic@gmx.at>
2022-01-07 11:25:38 +0100

    <fix>: Select the correct projection variable (=ts) for the initial version timestamp from the subselect  in getVersionInfos_f() and getVersionInfos_h().

--- src/jena_TDB/src/main/java/org/ai/wu/ac/at/tdbArchive/utils/QueryUtils.java
+++ src/jena_TDB/src/main/java/org/ai/wu/ac/at/tdbArchive/utils/QueryUtils.java
@@ -748,7 +748,7 @@ public final class QueryUtils {
 
 	public static final String getVersionInfos_f() {
 		String queryString = "Select (count(distinct ?ts) -1 as ?cnt_versions)\n" +
-				" (min(?valid_from) as ?initial_version_ts) where {\n" +
+				" (min(?ts) as ?initial_version_ts) where {\n" +
 				"    {\n" +
 				"        select \n" +
 				"        distinct (?valid_from as ?ts) where {\n" +
@@ -768,7 +768,7 @@ public final class QueryUtils {
 
 	public static final String getVersionInfos_h() {
 		String queryString = "Select (count(distinct ?ts) -1 as ?cnt_versions)\n" +
-				" (min(?valid_from) as ?initial_version_ts) where {\n" +
+				" (min(?ts) as ?initial_version_ts) where {\n" +
 				"    {\n" +
 				"        select \n" +
 				"        distinct (?valid_from as ?ts) where {\n" +
