47e0d170048cffec97abfb9b8645fc78f9e3ee80
Filip Kovacevic <f.kovacevic@gmx.at>
2022-01-12 12:56:54 +0100

    <chore>: Add requirements.txt

--- scripts/evaluation/run-docker-tdb.sh
+++ scripts/evaluation/run-docker-tdb.sh
@@ -26,7 +26,7 @@ case "$1" in
 esac
 
 
-policies="tb_star_h tb tb_star_f" # tb tb_star_h tb_star_f ic cb cbtb"
+policies="tb_star_f tb_star_h tb" # tb tb_star_h tb_star_f ic cb cbtb"
 categories="mat" # mat diff ver
 queries=$(cd ${querydir} && ls -v)
 
