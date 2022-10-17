a79d164e1ffb80fe95c5c183a1f67c0042374b48
Filip Kovacevic <f.kovacevic@gmx.at>
2022-10-17 13:47:43 +0200

    add script for experiment directoy setup

---
+++ scripts/0_create_experiment_dirs.sh
@@ -0,0 +1,6 @@
+mkdir -p ~/.BEAR/databases
+mkdir -p ~/.BEAR/output/logs
+mkdir -p ~/.BEAR/output/time
+mkdir -p ~/.BEAR/output/figures
+cp -R ../data/queries ~/.BEAR/queries 
+mkdir -p ~/.BEAR/rawdata
