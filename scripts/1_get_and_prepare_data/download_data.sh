7aa81f9c8733e4aeacc46a955e9bc5dbbc12f6ff
Filip Kovacevic <f.kovacevic@gmx.at>
2022-10-17 13:26:54 +0200

    fix bugs with gziping and taring into wrong directy

--- scripts/1_get_and_prepare_data/download_data.sh
+++ scripts/1_get_and_prepare_data/download_data.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 wget -t 3 -c -P ~/.BEAR/rawdata/beara https://aic.ai.wu.ac.at/qadlod/bear/BEAR_A/datasets/IC/alldata.IC.nt.tar.gz
-tar -xf ~/.BEAR/rawdata/beara/alldata.IC.nt.tar.gz
+tar -xf ~/.BEAR/rawdata/beara/alldata.IC.nt.tar.gz -C ~/.BEAR/rawdata/beara/alldata.IC.nt
 wget -t 3 -c -P ~/.BEAR/rawdata/beara https://aic.ai.wu.ac.at/qadlod/bear/BEAR_A/datasets/TB/alldata.TB.nq.gz
-gzip -d ~/.BEAR/rawdata/beara/alldata.TB.nq.gz
+gzip -d < ~/.BEAR/rawdata/beara/alldata.TB.nq.gz > ~/.BEAR/rawdata/beara/alldata.TB.nq
 
 wget -t 3 -c -P ~/.BEAR/rawdata/bearb-hour https://aic.ai.wu.ac.at/qadlod/bear/BEAR_B/datasets/hour/IC/alldata.IC.nt.tar.gz
-tar -xf ~/.BEAR/rawdata/bearb-hour/alldata.IC.nt.tar.gz
+tar -xf ~/.BEAR/rawdata/bearb-hour/alldata.IC.nt.tar.gz -C ~/.BEAR/rawdata/bearb-hour/alldata.IC.nt
 wget -t 3 -c -P ~/.BEAR/rawdata/bearb-hour https://aic.ai.wu.ac.at/qadlod/bear/BEAR_B/datasets/hour/TB/alldata.TB.nq.gz
-gzip -d ~/.BEAR/rawdata/bearb-hour/alldata.TB.nq.gz
+gzip -d < ~/.BEAR/rawdata/bearb-hour/alldata.TB.nq.gz > ~/.BEAR/rawdata/bearb-hour/alldata.TB.nq
 
 wget -t 3 -c -P ~/.BEAR/rawdata/bearb-day https://aic.ai.wu.ac.at/qadlod/bear/BEAR_B/datasets/day/IC/alldata.IC.nt.tar.gz
-tar -xf ~/.BEAR/rawdata/bearb-day/alldata.IC.nt.tar.gz
+tar -xf ~/.BEAR/rawdata/bearb-day/alldata.IC.nt.tar.gz -C ~/.BEAR/rawdata/bearb-day/alldata.IC.nt
 wget -t 3 -c -P ~/.BEAR/rawdata/bearb-day https://aic.ai.wu.ac.at/qadlod/bear/BEAR_B/datasets/day/TB/alldata.TB.nq.gz
-gzip -d ~/.BEAR/rawdata/bearb-day/alldata.TB.nq.gz
+gzip -d < ~/.BEAR/rawdata/bearb-day/alldata.TB.nq.gz > ~/.BEAR/rawdata/bearb-day/alldata.TB.nq
 
 wget -t 3 -c -P ~/.BEAR/rawdata/bearc https://aic.ai.wu.ac.at/qadlod/bear/BEAR_C/datasets/IC/alldata.IC.nt.tar.gz
-tar -xf ~/.BEAR/rawdata/bearc/alldata.IC.nt.tar.gz
+tar -xf ~/.BEAR/rawdata/bearc/alldata.IC.nt.tar.gz -C ~/.BEAR/rawdata/bearc/alldata.IC.nt
 wget -t 3 -c -P ~/.BEAR/rawdata/bearc https://aic.ai.wu.ac.at/qadlod/bear/BEAR_C/datasets/TB/alldata.TB.nq.gz
-gzip -d ~/.BEAR/rawdata/bearc/alldata.TB.nq.gz
+gzip -d < ~/.BEAR/rawdata/bearc/alldata.TB.nq.gz > ~/.BEAR/rawdata/bearc/alldata.TB.nq
