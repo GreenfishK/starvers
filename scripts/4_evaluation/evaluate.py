97359486693185f9fdfd27281958488d4af65c5b
Filip Kovacevic <f.kovacevic@gmx.at>
2022-10-21 17:26:26 +0200

    star experimenting with running the containers in parallel

--- scripts/4_evaluation/evaluate.py
+++ scripts/4_evaluation/evaluate.py
@@ -3,6 +3,8 @@ import multiprocessing
 from pathlib import Path
 import os
 import logging
+import pandas as pd
+import pexpect
 
 # header: tripleStore,snapshot,min,mean,max,stddev,count,sum
 # aggregation on tripleStore and version level
@@ -21,92 +23,92 @@ import logging
 
 # save dataset
 
-policies=["ic", "cb", "tb", "tbsf", "tbsh"]
-datasets=['beara', 'bearb-hour', 'bearb-day', 'bearc']
+df = pd.DataFrame(columns=['triplestore', 'dataset', 'policy', 'version', 'query_set', 'query', 'execution_time'])
+
+policies=['IC'] # ["IC", "CB", "TB", "TBSF", "TBSH"]
+datasets=['bearb-day', 'bearc'] # ['beara', 'bearb-hour', 'bearb-day', 'bearc']
 triple_stores=['graphdb', 'jenatdb2']
-final_queries=str(Path.home()) + "/.BEAR/queries/final_queries"
-ds_queries_map={'ic': {
-                    'beara': {'query_versions': 1, 'repositories': 58, 'queries': ['tb/queries_beara/high',
-                                                            'tb/queries_beara/low']}, 
-                    'bearb-day': {'query_versions': 1,'repositories': 89, 'queries': ['tb/queries_bearb-day/lookup',
-                                                                'tb/queries_bearb-day/join']}, 
-                    'bearb-hour': {'query_versions': 1, 'repositories': 1299, 'queries': ['tb/queries_bearb-day/lookup',
-                                                                'tb/queries_bearb-day/join']},
-                    'bearc': {'query_versions': 1, 'repositories': 32, 'queries': ['tb/queries_bearb-day/lookup',
-                                                            'tb/queries_bearb-day/join']}       
+# On host machine
+# final_queries=str(Path.home()) + "/.BEAR/queries/final_queries" 
+# In docker container
+final_queries= "/opt/starvers/eval/queries"
+ds_queries_map={'IC': {
+                    'beara': {'query_versions': 1, 'repositories': 58, 'queries': ['IC/queries_beara/high',
+                                                            'IC/queries_beara/low']}, 
+                    'bearb-day': {'query_versions': 1,'repositories': 89, 'queries': ['IC/queries_bearb/lookup',
+                                                                'IC/queries_bearb/join']}, 
+                    'bearb-hour': {'query_versions': 1, 'repositories': 1299, 'queries': ['IC/queries_bearb/lookup',
+                                                                'IC/queries_bearb/join']},
+                    'bearc': {'query_versions': 1, 'repositories': 32, 'queries': ['IC/queries_bearc']}       
                 },    
-                'cb': {
-                    'beara': {'query_versions': 1, 'repositories': 116, 'queries': ['tb/queries_beara/high',
-                                                            'tb/queries_beara/low']}, 
-                    'bearb-day': {'query_versions': 1, 'repositories': 178, 'queries': ['tb/queries_bearb-day/lookup',
-                                                                'tb/queries_bearb-day/join']}, 
-                    'bearb-hour': {'query_versions': 1, 'repositories': 2598, 'queries': ['tb/queries_bearb-day/lookup',
-                                                                'tb/queries_bearb-day/join']},
-                    'bearc': {'query_versions': 1, 'repositories': 64, 'queries': ['tb/queries_bearb-day/lookup',
-                                                            'tb/queries_bearb-day/join']}       
+                'CB': {
+                    'beara': {'query_versions': 1, 'repositories': 116, 'queries': ['CB/queries_beara/high',
+                                                            'CB/queries_beara/low']}, 
+                    'bearb-day': {'query_versions': 1, 'repositories': 178, 'queries': ['CB/queries_bearb/lookup',
+                                                                'CB/queries_bearb/join']}, 
+                    'bearb-hour': {'query_versions': 1, 'repositories': 2598, 'queries': ['CB/queries_bearb/lookup',
+                                                                'CB/queries_bearb/join']},
+                    'bearc': {'query_versions': 1, 'repositories': 64, 'queries': ['CB/queries_bearc']}       
                 },        
-                'tb':{
-                    'beara': {'query_versions': 58, 'repositories': 1, 'queries': ['tb/queries_beara/high',
-                                                            'tb/queries_beara/low']}, 
-                    'bearb-day': {'query_versions': 89, 'repositories': 1, 'queries': ['tb/queries_bearb-day/lookup',
-                                                                'tb/queries_bearb-day/join']}, 
-                    'bearb-hour': {'query_versions': 1299, 'repositories': 1, 'queries': ['tb/queries_bearb-day/lookup',
-                                                                'tb/queries_bearb-day/join']},
-                    'bearc': {'query_versions': 32, 'repositories': 1, 'queries': ['tb/queries_bearb-day/lookup',
-                                                            'tb/queries_bearb-day/join']}       
+                'TB':{
+                    'beara': {'query_versions': 58, 'repositories': 1, 'queries': ['TB/queries_beara/high',
+                                                            'TB/queries_beara/low']}, 
+                    'bearb-day': {'query_versions': 89, 'repositories': 1, 'queries': ['TB/queries_bearb-day/lookup',
+                                                                'TB/queries_bearb-day/join']}, 
+                    'bearb-hour': {'query_versions': 1299, 'repositories': 1, 'queries': ['TB/queries_bearb-hour/lookup',
+                                                                'TB/queries_bearb-hour/join']},
+                    'bearc': {'query_versions': 32, 'repositories': 1, 'queries': ['TB/queries_bearc']}       
                 },
-                'tbsf': {
-                    'beara': {'query_versions': 58, 'repositories': 1,'queries': ['tb/queries_beara/high',
-                                                            'tb/queries_beara/low']}, 
-                    'bearb-day': {'query_versions': 89, 'repositories': 1, 'queries': ['tb/queries_bearb-day/lookup',
-                                                                'tb/queries_bearb-day/join']}, 
-                    'bearb-hour': {'query_versions': 1299, 'repositories': 1, 'queries': ['tb/queries_bearb-day/lookup',
-                                                                'tb/queries_bearb-day/join']},
-                    'bearc': {'query_versions': 32, 'repositories': 1, 'queries': ['tb/queries_bearb-day/lookup',
-                                                            'tb/queries_bearb-day/join']}       
+                'TBSF': {
+                    'beara': {'query_versions': 58, 'repositories': 1,'queries': ['TBSF/queries_beara/high',
+                                                            'TBSF/queries_beara/low']}, 
+                    'bearb-day': {'query_versions': 89, 'repositories': 1, 'queries': ['TBSF/queries_bearb-day/lookup',
+                                                                'TBSF/queries_bearb-day/join']}, 
+                    'bearb-hour': {'query_versions': 1299, 'repositories': 1, 'queries': ['TBSF/queries_bearb-hour/lookup',
+                                                                'TBSF/queries_bearb-hour/join']},
+                    'bearc': {'query_versions': 32, 'repositories': 1, 'queries': ['TBSF/queries_bearc']}       
                 },
-                'tbsh': {
-                    'beara': {'query_versions': 58, 'repositories': 1, 'queries': ['tb/queries_beara/high',
-                                                            'tb/queries_beara/low']}, 
-                    'bearb-day': {'query_versions': 89, 'repositories': 1, 'queries': ['tb/queries_bearb-day/lookup',
-                                                                'tb/queries_bearb-day/join']}, 
-                    'bearb-hour': {'query_versions': 1299, 'repositories': 1, 'queries': ['tb/queries_bearb-day/lookup',
-                                                                'tb/queries_bearb-day/join']},
-                    'bearc': {'query_versions': 32, 'repositories': 1, 'queries': ['tb/queries_bearb-day/lookup',
-                                                            'tb/queries_bearb-day/join']}       
+                'TBSH': {
+                    'beara': {'query_versions': 58, 'repositories': 1, 'queries': ['TBSH/queries_beara/high',
+                                                            'TBSH/queries_beara/low']}, 
+                    'bearb-day': {'query_versions': 89, 'repositories': 1, 'queries': ['TBSH/queries_bearb-day/lookup',
+                                                                'TBSH/queries_bearb-day/join']}, 
+                    'bearb-hour': {'query_versions': 1299, 'repositories': 1, 'queries': ['TBSH/queries_bearb-day/lookup',
+                                                                'TBSH/queries_bearb-day/join']},
+                    'bearc': {'query_versions': 32, 'repositories': 1, 'queries': ['TBSH/queries_bearc']}       
                 }
 }
 
+
 def query_dataset(triple_store: str, policy: str, ds: str, port: int):
-    queries_to_execute = ds_queries_map[policy][ds]['queries']
+    query_sets = ds_queries_map[policy][ds]['queries']
     query_versions = ds_queries_map[policy][ds]['query_versions']
     repositories = ds_queries_map[policy][ds]['repositories']
 
     if triple_store == "graphdb":      
         for query_version in range(query_versions):
-            for query in queries_to_execute:
-                query_dir = final_queries + "/" + query  +  "/" + str(query_version)
-                for query_file_name in os.listdir(query_dir):
-                    if policy == "ic":
-                        for repository in range(1, repositories):
-                            repository_name = "{triple_store}_{policy}_{dataset}_{snapshot}".format(triple_store=triple_store, policy=policy, dataset=ds, snapshot=repository)
+            for query in query_sets:
+                query_set_version_dir = final_queries + "/" + query  +  "/" + str(query_version)
+                for query_file_name in os.listdir(query_set_version_dir):
+                    if policy == "IC":
+                        for repository in range(1, repositories+1):
+                            repository_name = "{policy}_{dataset}_{snapshot}".format(triple_store=triple_store, policy=policy.lower(), dataset=ds, snapshot=repository)
                             getEndpoint = "http://{hostname}:{port}/repositories/{repository_name}".format(hostname="Starvers", port=port, repository_name=repository_name)
                             postEndpoint = getEndpoint + "/statements"
                             engine = SPARQLWrapper(endpoint=getEndpoint, updateEndpoint=postEndpoint)
-                            with open(query_dir + "/" + query_file_name, "r") as file:
+                            with open(query_set_version_dir + "/" + query_file_name, "r") as file:
                                 query_text = file.read()
                                 engine.setQuery(query_text)
-                                logging.info("Querying repository {0}, on port {1} with query {2}". format(repository_name, port, query_file_name))
+                                print("Querying SPARQL endpoint {0} with query {1}". format(getEndpoint, query_file_name))
                                 result = engine.query()
                                 print(result)
                                 file.close()
-                    elif policy == "cb":
-                        logging.info("Not yet implemented")
-                    elif policy in ["tbsh", "tbsf", "tb"]:
-                        logging.info("Not yet implemented")
+                    elif policy == "CB":
+                        print("Not yet implemented")
+                    elif policy in ["TBSH", "TBSF", "TB"]:
+                        print("Not yet implemented")
     elif triple_store == "jenatdb2":
-        logging.info("Not yet implemented")
-        # TODO:  Run docker-compose
+        print("Not yet implemented")
 
 
 
@@ -114,22 +116,25 @@ def bulk_query():
     pass
     for triple_store in triple_stores:
         for policy in policies:
-            t1 = multiprocessing.Process(target=query_dataset, args=(triple_store, policy, datasets[0], 7200))
-            t2 = multiprocessing.Process(target=query_dataset, args=(triple_store, policy, datasets[1], 7300))
-            t3 = multiprocessing.Process(target=query_dataset, args=(triple_store, policy, datasets[2], 7400))
-            t4 = multiprocessing.Process(target=query_dataset, args=(triple_store, policy, datasets[3], 7500))
+            print(triple_store + " " + policy)
+            #t1 = multiprocessing.Process(target=query_dataset, args=(triple_store, policy, datasets[0], 7200))
+            #t2 = multiprocessing.Process(target=query_dataset, args=(triple_store, policy, datasets[1], 7210))
+            #t3 = multiprocessing.Process(target=query_dataset, args=(triple_store, policy, datasets[2], 7400))
+            #t4 = multiprocessing.Process(target=query_dataset, args=(triple_store, policy, datasets[3], 7500))
+            query_dataset(triple_store, policy, datasets[0], 7200)
+            query_dataset(triple_store, policy, datasets[0], 7210)
 
-            t1.start()
-            t2.start()
-            t3.start()
-            t4.start()
+            #t1.start()
+            #t2.start()
+            #t3.start()
+            #t4.start()
 
             # wait until threads are completely executed
-            t1.join()
-            t2.join()
-            t3.join()
-            t4.join()
+            #t1.join()
+            #t2.join()
+            #t3.join()
+            #t4.join()
 
             print("Done!")
 
-    
+bulk_query()
--- scripts/4_evaluation/run_triple_stores.sh
+++ scripts/4_evaluation/run_triple_stores.sh
@@ -1,17 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-policies="ic" # cb tbsf tbsh tb
-datasets="bearb-day bearc" # bearb-day beara bearc
+policies=("ic") # cb tbsf tbsh tb
+datasets=("bearb-day" "bearc") # bearb-day beara bearc
 host_port_graphdb=7200
 host_port_jenatdb2=3030
+command=""
 
+docker network create 4_evaluation_default
 ### GraphDB ##################################################################
 # Start 20 docker containers
-for policy in ${policies[@]}; do
-    for dataset in ${datasets[@]}; do
-        dataset=$dataset policy=$policy host_port=$host_port_graphdb \
-        docker-compose up graphdb_run
-        $host_port_graphdb = $host_port_graphdb + 10
+#for policy in ${policies[@]}; do
+#    for dataset in ${datasets[@]}; do
+#        dataset=$dataset policy=$policy host_port=$host_port_graphdb docker-compose up graphdb_run & 
+#        host_port_graphdb=$(($host_port_graphdb+10))
+#    done
+#done
+dataset=bearb-day policy=ic host_port=7200 docker-compose up -d graphdb_run & dataset=bearc policy=ic host_port=7210 docker-compose up -d graphdb_run & 
+
+
 
 ### JenaTDB2 #################################################################
 # TODO: write procedure for jenatdb2
@@ -20,4 +26,25 @@ for policy in ${policies[@]}; do
 
 ### Evaluate ################################################################
 # Run queries against each triplestore x dataset x policy, each stored in an own container (40 containers)
-# docker-compose up evaluate
+active_containers=0
+while [ $active_containers -ne $((${#policies[@]} * ${#datasets[@]})) ]; do
+    docker-compose logs graphdb_run | grep -w "Started GraphDB in workbench mode" > log.txt
+    active_containers=$((`sed -n '$=' log.txt`))
+done
+
+dataset=dummy policy=dummy host_port=7200 docker-compose up --build evaluate
+
+dataset=dummy policy=dummy host_port=7200 docker-compose down
+docker network rm 4_evaluation_default
+docker rmi -f $(docker images -f "dangling=true" -q).
+
+
+# TODO: free heap space in graphdb by deactivating the repository after querying it
+# start-console [OPTION] [repositoryID]
+# /opt/graphdb-free/app/bin/console
+# process1 = pexpect.spawnu("/opt/graphdb/dist/bin/console")
+# process1.timeout = 25
+# process1.sendline("Close {0}".format(repository_name))
+# process1.sendline("yes")
+# process1.expect("Closed repository '{0}'".format(repository_name))
+# process1.close()
