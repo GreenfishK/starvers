390a1885ea188b68aab611957769f7ef124f3620
Filip Kovacevic <f.kovacevic@gmx.at>
2022-10-23 16:10:39 +0200

    uncomment mkdirs

--- scripts/0_create_experiment_dirs.sh
+++ scripts/0_create_experiment_dirs.sh
@@ -1,7 +1,7 @@
-#mkdir -p ~/.BEAR/databases
-#mkdir -p ~/.BEAR/output/logs
-#mkdir -p ~/.BEAR/output/measurements
-#mkdir -p ~/.BEAR/output/figures
+mkdir -p ~/.BEAR/databases
+mkdir -p ~/.BEAR/output/logs
+mkdir -p ~/.BEAR/output/measurements
+mkdir -p ~/.BEAR/output/figures
 cp -R ../data/queries ~/.BEAR 
-#mkdir -p ~/.BEAR/rawdata
-#mkdir -p ~/.BEAR/configs
+mkdir -p ~/.BEAR/rawdata
+mkdir -p ~/.BEAR/configs
