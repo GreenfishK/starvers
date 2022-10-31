7bae8658206d5eb4d38ffc39891e4cad980dffb0
Filip Kovacevic <f.kovacevic@gmx.at>
2022-10-31 20:35:34 +0100

    update helloworld to include an example query

--- scripts_dev/4_evaluation/graphdb/src/main/java/com/ontotext/graphdb/example/app/hello/HelloWorld.java
+++ scripts_dev/4_evaluation/graphdb/src/main/java/com/ontotext/graphdb/example/app/hello/HelloWorld.java
@@ -1,7 +1,10 @@
 package com.ontotext.graphdb.example.app.hello;
 
 import com.ontotext.graphdb.example.util.EmbeddedGraphDB;
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.query.*;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.http.HTTPRepository;
 
 /**
  * Hello World app for GraphDB
@@ -12,6 +15,25 @@ public class HelloWorld {
         HTTPRepository repository = new HTTPRepository(args[0]);
         RepositoryConnection connection = repository.getConnection();
         System.out.println("Connection established");
+
+        // Preparing a SELECT query for later evaluation
+        TupleQuery tupleQuery = connection.prepareTupleQuery(QueryLanguage.SPARQL,
+        "SELECT ?s ?p ?o WHERE {" +
+                "?s ?p ?o ." +
+                "} limit 10");
+
+        TupleQueryResult tupleQueryResult = tupleQuery.evaluate();
+        while (tupleQueryResult.hasNext()) {
+            BindingSet bindingSet = tupleQueryResult.next();
+            for (Binding binding : bindingSet) {
+                // Each Binding contains the variable name and the value for this result row
+                String name = binding.getName();
+                Value value = binding.getValue();
+                System.out.println(name + " = " + value);
+            }
+        }
+
+        tupleQueryResult.close();
 
         if(args.length > 1 && args[1].equals("close")) {
             connection.close();
