380c0af329079765c6de51ffcfd6a7d19f7c34bd
Filip Kovacevic <f.kovacevic@gmx.at>
2022-11-03 17:20:45 +0100

    fix bug in ampersand correction

--- playground/correct.py
+++ playground/correct.py
@@ -3,6 +3,6 @@ import shutil
 
 with open("/home/fkovacev/Uni/PhD/Semantic_Technologies/RDFVersioningEvaluation/rdfostrich_BEAR/playground/input.nt") as fin, open("tmp_out.ttl", "w") as fout:
     for line in fin:
-        fout.write(re.sub(r'&(?!amp;)', r'&amp;', line))
+        fout.write(line.replace(r'<extref href=\"http://opac.kent.ac.uk/cgi-bin/Pwebrecon.cgi?DB=local&PAGE=First', r'<extref href=\"http://opac.kent.ac.uk/cgi-bin/Pwebrecon.cgi?DB=local&amp;PAGE=First'))
 fin.close()
 fout.close()
