8993477aa41e0e587bcdead31f1c01c4af044dbb
fkovacev@starvers <greenfish427@gmail.com>
2022-11-04 11:56:57 +0100

    add tools for testing the parsing of the BEARA dataset

--- playground/correct.py
+++ playground/correct.py
@@ -1,8 +1,18 @@
 import re
 import shutil
+from rdflib import Graph
 
-with open("/home/fkovacev/Uni/PhD/Semantic_Technologies/RDFVersioningEvaluation/rdfostrich_BEAR/playground/input.nt") as fin, open("tmp_out.ttl", "w") as fout:
-    for line in fin:
-        fout.write(line.replace(r'<extref href=\"http://opac.kent.ac.uk/cgi-bin/Pwebrecon.cgi?DB=local&PAGE=First', r'<extref href=\"http://opac.kent.ac.uk/cgi-bin/Pwebrecon.cgi?DB=local&amp;PAGE=First'))
+g = Graph()
+fout_path = "/mnt/data/starvers_eval/rawdata/beara/alldata.IC.nt/corrected/1c.nt"
+with open("/mnt/data/starvers_eval/rawdata/beara/alldata.IC.nt/1.nt") as fin, open(fout_path, "w") as fout:
+    print("Correcting...")
+    for i, line in enumerate(fin):
+        fout = open(fout_path, "w")
+        fout.write(line.replace(r' GMT+02:00"', r'+02:00"').replace(r'<extref href=\"http://opac.kent.ac.uk/cgi-bin/Pwebrecon.cgi?DB=local&PAGE=First', r'<extref href=\"http://opac.kent.ac.uk/cgi-bin/Pwebrecon.cgi?DB=local&amp;PAGE=First'))
+        fout.close()
+        print("Parsing line {0}: {1}".format(i, line))
+        g.parse(fout_path)        
 fin.close()
-fout.close()
+
+
+
