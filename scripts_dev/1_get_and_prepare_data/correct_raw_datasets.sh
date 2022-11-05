5fca1a98d5d7befe98220bfaac6546c7d2dd3c8b
Filip Kovacevic <f.kovacevic@gmx.at>
2022-11-05 12:59:28 +0100

    add general ampersand corrections for www.kent.ac.uk

--- scripts_dev/1_get_and_prepare_data/correct_raw_datasets.sh
+++ scripts_dev/1_get_and_prepare_data/correct_raw_datasets.sh
@@ -3,9 +3,10 @@
 # Variables
 baseDir=/starvers_eval
 SCRIPT_DIR=/starvers_eval/scripts
-policies=("ic tb") # only raw datasets
+policies=("ic" "tb") # only raw datasets
 datasets=("${datasets}") # beara bearb_hour bearb_day bearc
 
+echo "Start corrections"
 for policy in ${policies[@]}; do
     case $policy in 
         ic) datasetDirOrFile=alldata.IC.nt;;
@@ -15,9 +16,7 @@ for policy in ${policies[@]}; do
             exit 2
         ;;
     esac
-
     for dataset in ${datasets[@]}; do
-        echo $dataset
         case $dataset in 
             beara) versions=1 file_name_struc="%01g";; #versions=58
             bearb_hour) versions=1299 file_name_struc="%06g";; 
@@ -28,27 +27,41 @@ for policy in ${policies[@]}; do
                 exit 2
             ;;
         esac
-        if [ "$policy" == "ic"]; then
-            if [ "$dataset" == "beara"]; then
-                # Correct BEARA IC
+        if [ "$policy" == "ic" ]; then
+            if [ "$dataset" == "beara" ]; then
+                echo "Correcting $dataset for $policy policy"
                 for c in $(seq -f $file_name_struc 1 ${versions})
                 do
-                    ic_file = $baseDir/rawdata/$dataset/$datasetDirOrFile/${c}.nt
-                    # Bad timestamp format
-                    sed -i 's/ GMT+02:00"/+02:00/g' ic_file
-                    # Ampersand not escaped
-                    sed -i 's/<extref href=\\"http:\/\/opac.kent.ac.uk\/cgi-bin\/Pwebrecon.cgi?DB=local\&PAGE=First/<extref href=\\"http:\/\/opac.kent.ac.uk\/cgi-bin\/Pwebrecon.cgi?DB=local\&amp;PAGE=First/g'
-                    # Bad blank nodes format
-                    sed -i 's/\(<\)\(node.*\)\(>\)/_:\2/g'
+                    ic_file=$baseDir/rawdata/$dataset/$datasetDirOrFile/${c}.nt
+                    echo "$ic_file"
+                    echo "Correct bad timestamp format"
+                    sed -i 's/ GMT+02:00"/+02:00"/g' $ic_file
+                    echo "Correct unescaped ampersand"
+                    sed -i -r 's/(href=\\.*\?.*)(\&)(amp;){0,1}/\1\&amp;/g' $ic_file
+                    sed -i -r 's/(<extref href=\\"http:\/\/www.kent.ac.uk\/library\/specialcollections\/other\/search.html\?k\[0\]=PC)(\&)(amp;){0,1}/\1\&amp;/g' $ic_file
+                    echo "Correct bad blank nodes format"
+                    sed -i 's/\(<\)\(node.*\)\(>\)/_:\2/g' $ic_file
+                    # Unrecognised ISO 8601 date format: '-0038'
+                    # Unrecognised ISO 8601 time format: '8:00:00'
+                    # Unrecognised ISO 8601 date format: '5-06-30'
+                    # Unrecognised ISO 8601 date format: '2-07-01'
+                    # ISO 8601 time designator 'T' missing. Unable to parse datetime string '2008-06-18'
+                    # ISO 8601 time designator 'T' missing. Unable to parse datetime string '2008-08-22'
+                    # ISO 8601 time designator 'T' missing. Unable to parse datetime string '2009-05-26'
+                    # ISO 8601 time designator 'T' missing. Unable to parse datetime string '2009-06-15'
+                    # prefix must not be bound to one of the reserved namespace names:
                 done
-            elif [ "$dataset" == "bearc"]; then
-                # Correct BEARC IC
+            elif [ "$dataset" == "bearc" ]; then
+                echo "Correcting $dataset for $policy policy"
                 for c in $(seq -f $file_name_struc 1 ${versions})
                 do
-                # Bad IRI
-                sed -i 's/<http:\/cordis.europa.eu\/data\/cordis-fp7projects-xml.zip>/<http:\/\/cordis.europa.eu\/data\/cordis-fp7projects-xml.zip>/g' ic_file
+                    ic_file=$baseDir/rawdata/$dataset/$datasetDirOrFile/${c}.nt
+                    echo "$ic_file"
+                    echo "Correct bad IRI"
+                    sed -i 's/<http:\/cordis.europa.eu\/data\/cordis-fp7projects-xml.zip>/<http:\/\/cordis.europa.eu\/data\/cordis-fp7projects-xml.zip>/g' $ic_file
                 done
             fi
         fi
+    done
 done
 
