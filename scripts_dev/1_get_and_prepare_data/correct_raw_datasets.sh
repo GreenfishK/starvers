7f07987901d0fae000de185177c45a7daa07de42
Filip Kovacevic <f.kovacevic@gmx.at>
2022-11-16 16:09:35 +0100

    refactor regex

--- scripts_dev/1_get_and_prepare_data/correct_raw_datasets.sh
+++ scripts_dev/1_get_and_prepare_data/correct_raw_datasets.sh
@@ -50,17 +50,7 @@ for policy in ${policies[@]}; do
                     sed -i 's/ GMT+02:00"/+02:00"/g' $ic_file
 
                     echo "Change datatype of wrongly formatted datatypes to string."
-                    sed -i -r 's/("-[0-9]{4}-[0-9]{2}-[0-9]{2}"\^\^<http:\/\/www.w3.org\/2001\/XMLSchema#)(date>)/\1string>/g' $ic_file # 
-                    sed -i -r 's/("[A-Za-z]{0,20}\s([0-9]{2},\s){0,1}[0-9]{4}"\^\^<http:\/\/www.w3.org\/2001\/XMLSchema#)(date>)/\1string>/g' $ic_file #
-                    sed -i -r 's/("[0-9]+"\^\^<http:\/\/www.w3.org\/2001\/XMLSchema#)(date>|dateTime>)/\1string>/g' $ic_file #
-                    sed -i -r 's/("[0-9]*[A-Za-z\\-\\#/]+[^"]*"\^\^<http:\/\/www.w3.org\/2001\/XMLSchema#)(date>|dateTime>)/\1string>/g' $ic_file #
-                    sed -i -r 's/("0[^"]*"\^\^<http:\/\/www.w3.org\/2001\/XMLSchema#)(date>|dateTime>)/\1string>/g' $ic_file #
-                    sed -i -r 's/("[A-Za-z]+.*"\^\^<http:\/\/www.w3.org\/2001\/XMLSchema#)(date>|dateTime>)/\1string>/g' $ic_file
-                    sed -i -r 's/("-[0-9]{4}"\^\^<http:\/\/www.w3.org\/2001\/XMLSchema#)(gYear>)/"\1string>/g' $ic_file
-                    sed -i -r 's/("[^"]*[^A-Za-z0-9]+[^"]*"\^\^<http:\/\/www.w3.org\/2001\/XMLSchema#)(hexBinary>)/\1string>/g' $ic_file #
-                    sed -i -r 's/(""\^\^<http:\/\/www.w3.org\/2001\/XMLSchema#)(integer>|dateTime>|double>|int>|duration>)/\1string>/g' $ic_file
-                    sed -i -r 's/(".*"\^\^<http:\/\/www.w3.org\/2001\/XMLSchema#)(int>|double>)/\1string>/g' $ic_file
-                    sed -i -r 's/(\^\^<http:\/\/www.w3.org\/1999\/02\/22-rdf-syntax-ns#XMLLiteral>)/\^\^<http:\/\/www.w3.org\/2001\/XMLSchema#string>/g' $ic_file
+                    sed -i -r 's/("(-[0-9]{4}-[0-9]{2}-[0-9]{2}|[A-Za-z]{0,20}\s([0-9]{2},\s){0,1}[0-9]{4}|[0-9]+|[0-9]*[A-Za-z?!\\#@/-]+[^"]*|0[^"]*|[A-Za-z]+.*)"\^\^<http:\/\/www.w3.org\/2001\/XMLSchema#)(date>|dateTime>)/\1string>/g' $ic_file
                     
                     echo "Correct unescaped ampersand"  
                     sed -i -r 's/(href=\\.*\?.*)(\&)(amp;){0,1}/\1\&amp;/g' $ic_file
@@ -75,7 +65,6 @@ for policy in ${policies[@]}; do
                     sed -i -r 's/(^<)(#[^>]*> <.*> (<.*>|".*"(\^\^<.*>){0,1}) .$)/\1http:\/\/example\.com\2/g' $ic_file
                     sed -i -r 's/(<)(node[A-Za-z0-9]*)(>)/_:\2/g' $ic_file
 
-                
                     echo "Corrected $ic_file \n" >> $baseDir/output/logs/corrections.txt
                 done
             elif [ "$dataset" == "bearb_hour" ]; then
