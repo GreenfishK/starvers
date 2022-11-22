fbe216152419cb5c6d7b54fc4ddeec0dc3af7eb9
Filip Kovacevic <f.kovacevic@gmx.at>
2022-11-22 15:47:19 +0100

    fix bug in counting new lines

--- scripts_dev/1_get_and_prepare_data/construct_datasets.py
+++ scripts_dev/1_get_and_prepare_data/construct_datasets.py
@@ -46,13 +46,13 @@ def construct_change_sets(dataset_dir: str, end_vers: int, format: str, zf: int)
         assert len(ic2) - len(ic1) == len(cs_added) - len(cs_deleted)
 
         cs_added_str = "\n".join(triple for triple in cs_added if not triple.startswith("#"))
-        print("Create data-added_{0}-{1}.nt with {2} triples.".format(i, i + 1, cs_added_str.count("\n")))
+        print("Create data-added_{0}-{1}.nt with {2} triples.".format(i, i + 1, cs_added_str.count("\n") + 1))
         with open(cb_comp_dir + "/" + "data-added_{0}-{1}.{2}".format(i, i + 1, format), "w") as cs_added_file:
             cs_added_file.write(cs_added_str)
         cs_added, cs_added_str = None, None
 
         cs_deleted_str = "\n".join(triple for triple in cs_deleted if not triple.startswith("#"))
-        print("Create data-deleted_{0}-{1}.nt with {2} triples.".format(i, i + 1, cs_deleted_str.count("\n")))
+        print("Create data-deleted_{0}-{1}.nt with {2} triples.".format(i, i + 1, cs_deleted_str.count("\n") + 1))
         with open(cb_comp_dir + "/" + "data-deleted_{0}-{1}.{2}".format(i, i + 1, format), "w") as cs_deleted_file:
             cs_deleted_file.write(cs_deleted_str)
         cs_deleted, cs_deleted_str = None, None
