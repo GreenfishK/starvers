d232c8c5807a4c3353fb04ca5636a6d3722cdb01
Filip Kovacevic <f.kovacevic@gmx.at>
2022-11-23 14:53:05 +0100

    debug

--- scripts_dev/1_get_and_prepare_data/construct_datasets.py
+++ scripts_dev/1_get_and_prepare_data/construct_datasets.py
@@ -121,11 +121,10 @@ def construct_tb_star_ds(source_ic0, source_cs: str, destination: str, last_vers
             
             deleted_triples_raw = open(source_cs + "/" + filename, "r").read().split(" .\n")
             for i, triple in enumerate(added_triples):
-                print(triple)
                 if len(deleted_triples_raw) == 0:
                     break
                 if triple[1] == deleted_triples_raw[0]:
-                    print(i, triple[1])
+                    print("yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeees", i, triple[1])
                     added_triples[i][7] = vers_ts_str
                     deleted_triples_raw.pop(0)
 
