ba53d55c726335b133ad6a979f8f96d389564a29
Filip Kovacevic <f.kovacevic@gmx.at>
2022-11-30 00:47:32 +0100

    fix bug

--- scripts_dev/1_get_and_prepare_data/correct_raw_datasets.sh
+++ scripts_dev/1_get_and_prepare_data/correct_raw_datasets.sh
@@ -65,7 +65,7 @@ for dataset in ${datasets[@]}; do
             while : ; do   
                 invalid_line=`/jena-fuseki/tdbloader2 --loc ${baseDir}/databases/preprocessing/jenatdb2_${policy}_${dataset}/${repositoryID} $ds_abs_path_tmp | grep -Po '(?<=ERROR riot            :: \[line: )[0-9]+'`
                 [[ ! -z "$invalid_line" ]] || break
-                sed -i "0,{$invalid_line}d" $ds_abs_path_tmp
+                sed -i "1,{$invalid_line}d" $ds_abs_path_tmp
                 invalid_line_cnt=$(($invalid_line_cnt+$invalid_line))
                 echo "$invalid_line_cnt" >> $baseDir/output/logs/preprocessing/invalid_triples_${repositoryID}.txt   
                 #sed -i -r "${invalid_line}s/(.*)/# \1/g" $ds_abs_path_tmp      
