625928b718f07ab0825adf1124917ad77ae48522
Filip Kovacevic <f.kovacevic@gmx.at>
2022-12-05 11:29:20 +0100

    update logger

--- scripts_dev/2_preprocess/construct_datasets.py
+++ scripts_dev/2_preprocess/construct_datasets.py
@@ -153,7 +153,7 @@ def construct_tb_star_ds(source_ic0, source_cs: str, destination: str, init_time
                     result_set[i][7] = vers_ts_str
                     deleted_triples_raw.pop(0)
                 if round((i/len(result_set))*100) % 10 == 0:
-                    logging.info("Processed {0}%".format(round((i/len(result_set))*100)))
+                    logging.info("{0}% artificial valid_until timestamps updated.".format(round((i/len(result_set))*100)))
         
     logging.info("The final RDF-star dataset has {0} triples".format(len(result_set)))
     logging.info("Write RDF-star dataset from memory to file.")
