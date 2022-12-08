275456616414d2fd6fcb8d7c23460c5dd88c81a9
Filip Kovacevic <f.kovacevic@gmx.at>
2022-12-08 11:29:59 +0100

    remove logging message

--- src/starvers/starvers.py
+++ src/starvers/starvers.py
@@ -461,12 +461,6 @@ class TripleStoreEngine:
         logging.info("Inserting triples as batches of 1000 triples.")
         for i in range(0, len(insert_block), 1000):
             insert_batch = "\n".join(insert_block[i:min(i+1000, len(insert_block))])
-            # Surround blank nodes in the subject position with pointy brackets
-            #insert_batch = re.sub(r'(?:(?<=\s\()|(?<=^\())\s*_:([a-zA-Z0-9]+)', r'<_:\1>', insert_batch)
-            # Surround blank nodes in the object position with pointy brackets
-            #insert_batch = re.sub(r'_:([a-zA-Z0-9]+)\s*(?=\)(\s|$))', r'<_:\1>', insert_batch)
-            logging.info(insert_batch)
-
             if timestamp:
                 version_timestamp = versioning_timestamp_format(timestamp)
                 insert_statement = statement.format(sparql_prefixes, insert_batch, '"' + version_timestamp + '"')
@@ -579,10 +573,6 @@ class TripleStoreEngine:
         logging.info("Outdating triples as batches of 1000 triples.")
         for i in range(0, len(outdate_block), 1000):
             outdate_batch = "\n".join(outdate_block[i:min(i+1000, len(outdate_block))])
-            # Surround blank nodes in the subject position with pointy brackets
-            #outdate_batch = re.sub(r'(?:(?<=\s\()|(?<=^\())\s*_:([a-zA-Z0-9]+)', r'<_:\1>', outdate_batch)
-            # Surround blank nodes in the object position with pointy brackets
-            #outdate_batch = re.sub(r'_:([a-zA-Z0-9]+)\s*(?=\)(\s|$))', r'<_:\1>', outdate_batch)
             if timestamp:
                 version_timestamp = versioning_timestamp_format(timestamp)
                 outdate_statement = statement.format(sparql_prefixes, outdate_batch, '"' + version_timestamp + '"')
