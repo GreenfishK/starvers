7386d2225729ef47ec6fa1c213ae362a9df9ebe5
Filip Kovacevic <greenfish427@gmail.com>
2022-12-28 21:54:55 +0100

    add logging for bash

--- scripts_dev/2_preprocess/start_jenatdb2.sh
+++ scripts_dev/2_preprocess/start_jenatdb2.sh
@@ -7,23 +7,25 @@ reset=$4
 
 # Set variables
 script_dir=/starvers_eval/scripts
+log_file=/starvers_eval/output/logs/preprocessing/construct_datasets.txt
+log_timestamp() { date +%Y-%m-%d\ %A\ %H:%M:%S; }
+log_level="root:INFO"
+
 #jenatdb2_port=$((3030))
 export JAVA_HOME=/usr/local/openjdk-11
 export PATH=/usr/local/openjdk-11/bin:$PATH
-#export FUSEKI_HOME=/jena-fuseki
-#export _JAVA_OPTIONS="-Xmx90g -Xms90g"
-#export ADMIN_PASSWORD=starvers
 
 if [ $reset == "true" ]; then
-    echo "Clean repositories..."
+    echo "$(log_timestamp) ${log_level}:Clean repositories..." >> $log_file
     rm -rf /starvers_eval/databases/preprocessing/
+    rm -rf /starvers_eval/configs/preprocessing/jenatdb2_${policy}_${dataset}
     rm -rf /run/configuration
 
-    echo "Create directories..."
+    echo "$(log_timestamp) ${log_level}:Create directories..." >> $log_file
     mkdir -p /starvers_eval/configs/preprocessing/jenatdb2_${policy}_${dataset}
     mkdir -p /run/configuration
 
-    echo "Parametrize and copy config file..."
+    echo "$(log_timestamp) ${log_level}:Parametrize and copy config file..." >> $log_file
     repositoryID=${policy}_${dataset}
     cp ${script_dir}/2_preprocess/configs/jenatdb2-config_template.ttl /starvers_eval/configs/preprocessing/jenatdb2_${policy}_${dataset}/${repositoryID}.ttl
     sed -i "s/{{repositoryID}}/$repositoryID/g" /starvers_eval/configs/preprocessing/jenatdb2_${policy}_${dataset}/${repositoryID}.ttl
@@ -32,17 +34,17 @@ if [ $reset == "true" ]; then
 fi
 
 if [ $ingest == "true" ]; then
-    echo "Ingest empty dataset..."
+    echo "$(log_timestamp) ${log_level}:Ingest empty dataset..." >> $log_file
     /jena-fuseki/tdbloader2 --loc /starvers_eval/databases/preprocessing/jenatdb2_${policy}_${dataset}/${repositoryID} /starvers_eval/rawdata/${dataset}/empty.nt
 fi
 
-echo "Start database server in background..."
+echo "$(log_timestamp) ${log_level}:Start database server in background..." >> $log_file
 cp /starvers_eval/configs/preprocessing/jenatdb2_${policy}_${dataset}/*.ttl /run/configuration
 nohup /jena-fuseki/fuseki-server --port=3030 --tdb2 &
 
 # Wait until server is up
-echo "Waiting..."
+echo "$(log_timestamp) ${log_level}:Waiting..." >> $log_file
 while [[ $(curl -I http://Starvers:3030 2>/dev/null | head -n 1 | cut -d$' ' -f2) != '200' ]]; do
     sleep 1s
 done
-echo "Fuseki server is up"
+echo "$(log_timestamp) ${log_level}:Fuseki server is up" >> $log_file
