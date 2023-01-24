241d8d1e9df956c2af0e46d7e6de70bff834ed05
Filip Kovacevic <greenfish427@gmail.com>
2023-01-24 16:26:53 +0100

    fix bug

--- scripts_dev/6_evaluate/evaluate.sh
+++ scripts_dev/6_evaluate/evaluate.sh
@@ -11,7 +11,7 @@ log_file=/starvers_eval/output/logs/evaluate/query.txt
 log_timestamp() { date +%Y-%m-%d\ %A\ %H:%M:%S; }
 log_level="root:INFO"
 mkdir -p /starvers_eval/output/logs/evaluate/
-> log_file
+> $log_file
 > /starvers_eval/output/measurements/time.csv
 
 # main loop
