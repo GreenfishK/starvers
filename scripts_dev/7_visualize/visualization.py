9df4e50694b574cd18e8fd42266a4ad9298e342b
Filip Kovacevic <greenfish427@gmail.com>
2023-01-26 17:38:45 +0100

    add toml file for evaluation setup

--- scripts_dev/7_visualize/visualization.py
+++ scripts_dev/7_visualize/visualization.py
@@ -18,6 +18,86 @@ datasets = sys.argv[2].split(" ")
 if not os.path.exists(figures_out):
     os.makedirs(figures_out)
 
+# Plots
+# Plot1: filters: graphdb, bearb_day, lookup
+# Plot1: measures: query execution time + snapshot creation time
+# Plot1: x-axis: snapshots
+# Plot1: lines: policy
+
+# Plot2: filters: graphdb, bearb_day, join 
+# Plot2: measures: query execution time + snapshot creation time
+# Plot2: x-axis: snapshots
+# Plot2: lines: policy
+
+# Plot3: filters: graphdb, bearb_hour, lookup 
+# Plot3: measures: query execution time + snapshot creation time
+# Plot3: x-axis: snapshots
+# Plot3: lines: policy
+
+# Plot4: filters: graphdb, bearb_hour, join 
+# Plot4: measures: query execution time + snapshot creation time
+# Plot4: x-axis: snapshots
+# Plot4: lines: policy
+
+# Plot5: filters: graphdb, bearc, complex
+# Plot5: measures: query execution time + snapshot creation time
+# Plot5: x-axis: snapshots
+# Plot5: lines: policy
+
+# Plot6: filters: graphdb, bearb_day
+# Plot6: measures: ingestion time, raw file size in MiB, db file size in MiB
+# Plot6: x-axis: policy
+# Plot6: type: bars
+
+# Plot7: filters: graphdb, bearb_hour
+# Plot7: measures: ingestion time, raw file size in MiB, db file size in MiB
+# Plot7: x-axis: policy
+# Plot7: type: bars
+
+# Plot8: filters: graphdb, bearc
+# Plot8: measures: ingestion time, raw file size in MiB, db file size in MiB
+# Plot8: x-axis: policy
+# Plot8: type: bars
+
+# Plot9: filters: jenatdb2, bearb_day, lookup
+# Plot9: measures: query execution time + snapshot creation time
+# Plot9: x-axis: snapshots
+# Plot9: lines: policy
+
+# Plot10: filters: jenatdb2, bearb_day, join 
+# Plot10: measures: query execution time + snapshot creation time
+# Plot10: x-axis: snapshots
+# Plot10: lines: policy
+
+# Plot11: filters: jenatdb2, bearb_hour, lookup 
+# Plot11: measures: query execution time + snapshot creation time
+# Plot11: x-axis: snapshots
+# Plot11: lines: policy
+
+# Plot12: filters: jenatdb2, bearb_hour, join 
+# Plot12: measures: query execution time + snapshot creation time
+# Plot12: x-axis: snapshots
+# Plot12: lines: policy
+
+# Plot13: filters: jenatdb2, bearc, complex
+# Plot13: measures: query execution time + snapshot creation time
+# Plot13: x-axis: snapshots
+# Plot13: lines: policy
+
+# Plot14: filters: jenatdb2, bearb_day
+# Plot14: measures: ingestion time, raw file size in MiB, db file size in MiB
+# Plot14: x-axis: policy
+# Plot14: type: bars
+
+# Plot15: filters: jenatdb2, bearb_hour
+# Plot15: measures: ingestion time, raw file size in MiB, db file size in MiB
+# Plot15: x-axis: policy
+# Plot15: type: bars
+
+# Plot16: filters: jenatdb2, bearc
+# Plot16: measures: ingestion time, raw file size in MiB, db file size in MiB
+# Plot16: x-axis: policy
+# Plot16: type: bars
 
 def plot_lookup_queries(timestamp: datetime, triple_store: str, triple_patterns: list):
     """
