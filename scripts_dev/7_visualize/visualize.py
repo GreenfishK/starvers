a485a1cdbed6c536276db3d48f2cf316ab4376d9
Filip Kovacevic <greenfish427@gmail.com>
2023-02-07 18:27:00 +0100

    update visualize

--- scripts_dev/7_visualize/visualize.py
+++ scripts_dev/7_visualize/visualize.py
@@ -147,9 +147,7 @@ def create_query_performance_plots(triplestore: str):
 
 def create_plots(dataset: str, query_set: str):
     """
-    Create a figure with 4 subplots, one for each dataset. Each subplot shows 4 lines, one for each policy. 
-    On the x-axis the snapshots are shown and on the y-axis the mean total execution time aggregated 
-    on policy and snapshot level.
+
 
     """
     # Data
@@ -183,8 +181,8 @@ def create_plots(dataset: str, query_set: str):
             ax.plot(policy_df['snapshot'], policy_df['execution_time_total'], label=policy, color=color_map[policy])
         
         ax.set_title(f"{triplestore}")
-        ax.set_xlabel('snapshot')
-        ax.set_ylabel('execution_time_total')
+        ax.set_xlabel('snapshots')
+        ax.set_ylabel('Execution time (s)')
         tick_steps = max(math.floor(len(policy_df['snapshot'])/10), 1)
         ax.set_xticks(ticks=range(0, len(policy_df['snapshot']), tick_steps),
                     labels=[*range(0, len(policy_df['snapshot']), tick_steps)])
@@ -229,7 +227,7 @@ def create_plots(dataset: str, query_set: str):
     #purple_patch = mpatches.Patch(color='purple', label='tb_sr_rs')
     #fig.legend(loc="upper right", handles={red_patch, blue_patch, green_patch, purple_patch})
 
-    fig.suptitle(f"{dataset} and {query_set} query performance and ingest plots", fontsize=32)
+    fig.suptitle(f"Query performance and data ingestion & storage plots for {dataset} and {query_set}", fontsize=32)
     fig.set_figheight(9)
     fig.set_figwidth(16)
 
@@ -238,9 +236,107 @@ def create_plots(dataset: str, query_set: str):
     plt.close()
 
     
+def create_plots2(triplestore: str, dataset: str):
+    """
 
-create_plots("bearb_hour", "lookup")
-create_plots("bearb_hour", "join")
-create_plots("bearb_day", "lookup")
-create_plots("bearb_day", "join")
-create_plots("bearbc", "complex")
+
+    """
+    # Data
+    performance_data = pd.read_csv(measurements_in + "time.csv", delimiter=";", decimal=".",
+                            dtype={"triplestore": "category", "dataset": "category", "policy": "category",
+                            "query_set": "category", "snapshot": "string", "query": "string",
+                            "execution_time": "float", "snapshot_creation_time": "float"})
+    performance_data['snapshot_ts'] = pd.to_datetime(performance_data['snapshot_ts'])
+    performance_data['execution_time_total'] = performance_data['execution_time'] + performance_data['snapshot_creation_time']
+    performance_data = performance_data[['triplestore', 'dataset', 'policy', 'snapshot', 'query_set', 'execution_time_total']]
+
+    ingestion_data = pd.read_csv(measurements_in + "ingestion.csv", delimiter=";", decimal=".")
+
+    # Parameters
+    policies = ['ic_sr_ng', 'cb_sr_ng', 'tb_sr_ng', 'tb_sr_rs']
+    colors = ['red', 'blue', 'green', 'purple']
+    color_map = dict(zip(policies, colors))
+    
+    # Plot variables
+    #fig, axs = plt.subplots(2, 2, figsize=(15, 10))
+    #axs = axs.flatten()
+
+    fig = plt.figure()
+    gs = fig.add_gridspec(2,2)
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax2 = fig.add_subplot(gs[0, 1])
+    ax3 = fig.add_subplot(gs[1, :])
+    
+       
+    def plot_performance(query_set: str, ax):
+        dataset_df = performance_data[(performance_data['triplestore'] == triplestore) & (performance_data['dataset'] == dataset) & (performance_data['query_set'] == query_set)]
+        means = dataset_df.groupby(['policy', 'snapshot']).mean()
+        means = means.reset_index()
+        
+        for policy in policies:
+            policy_df = means[means['policy'] == policy]
+            ax.plot(policy_df['snapshot'], policy_df['execution_time_total'], label=policy, color=color_map[policy])
+        
+        ax.set_title(f"{triplestore}")
+        ax.set_xlabel('snapshots')
+        ax.set_ylabel('Execution time (s)')
+        tick_steps = max(math.floor(len(policy_df['snapshot'])/10), 1)
+        ax.set_xticks(ticks=range(0, len(policy_df['snapshot']), tick_steps),
+                    labels=[*range(0, len(policy_df['snapshot']), tick_steps)])
+
+    plot_performance(query_set="lookup", ax=ax1)
+    plot_performance(query_set="join", ax=ax2)
+
+    def plot_ingestion(ax):
+        ax2 = ax.twinx()
+        bar_width = 0.2
+        opacity = 1
+        index = range(len(policies))
+
+        for i, policy in enumerate(policies):
+            
+            policy_data = ingestion_data[(ingestion_data['triplestore'] == triplestore) & (ingestion_data["policy"] == policy) & (ingestion_data['dataset'] == dataset)]
+            ing_time = policy_data["ingestion_time"].mean()
+            raw_size = policy_data["raw_file_size_MiB"].mean()
+            db_size = policy_data["db_files_disk_usage_MiB"].mean()
+            
+            ax.bar(i - bar_width, ing_time, bar_width, alpha=opacity, color='blue', label="Ingestion Time")
+            ax2.bar(i, raw_size, bar_width, alpha=opacity, color='limegreen', label="Raw File Size")
+            ax2.bar(i, db_size, bar_width * 0.6, alpha=opacity, color='darkgreen', label="DB File Size")
+        
+        ax.set_xticks(index)
+        ax.yaxis.label.set_color('blue')
+        ax.set_xticklabels(policies)
+        #ax.set_title("")
+        #ax.set_xlabel("Policy")
+        ax.set_ylabel("Ingestion Time (s)")
+        ax2.set_ylabel("Storage Consumption (MiB)")
+        ax2.yaxis.label.set_color('darkgreen')
+
+    plot_ingestion(ax=ax3)
+
+    
+    # Add legend
+    #red_patch = mpatches.Patch(color='red', label='ic_sr_ng')
+    #blue_patch = mpatches.Patch(color='blue', label='cb_sr_ng')
+    #green_patch = mpatches.Patch(color='green', label='tb_sr_ng')
+    #purple_patch = mpatches.Patch(color='purple', label='tb_sr_rs')
+    #fig.legend(loc="upper right", handles={red_patch, blue_patch, green_patch, purple_patch})
+
+    fig.suptitle(f"Query performance and data ingestion & storage plots for {triplestore} and {dataset}", fontsize=32)
+    fig.set_figheight(9)
+    fig.set_figwidth(16)
+
+    plt.tight_layout(pad=3.0, w_pad=2, h_pad=1.0)
+    plt.savefig(f"/starvers_eval/output/figures/time_{triplestore}_{dataset}.png")
+    plt.close()
+
+
+
+#create_plots("bearb_hour", "lookup")
+#create_plots("bearb_hour", "join")
+#create_plots("bearb_day", "lookup")
+#create_plots("bearb_day", "join")
+#create_plots("bearbc", "complex")
+
+create_plots2("graphdb", "bearb_hour")
