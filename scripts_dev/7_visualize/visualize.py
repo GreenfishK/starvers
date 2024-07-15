5a4cb9f26f1e1a908013714ced6b47b721c6f9da
Filip Kovacevic <greenfish427@gmail.com>
2024-07-15 15:09:25 +0200

    update plots

--- scripts_dev/7_visualize/visualize.py
+++ scripts_dev/7_visualize/visualize.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
 import numpy as np
 from datetime import datetime
 from datetime import timezone
@@ -31,11 +32,9 @@ datasets = sys.argv[2].split(" ")
 
 ############################################# Visualize #############################################
 
-    
 def create_plots(triplestore: str, dataset: str):
     """
-
-
+    Create and save performance and ingestion plots for the specified triplestore and dataset.
     """
     # Data
     performance_data = pd.read_csv(measurements_in + "time.csv", delimiter=";", decimal=".",
@@ -54,27 +53,32 @@ def create_plots(triplestore: str, dataset: str):
 
     # Figure and axes for query performance and ingestion
     fig = plt.figure()
-    gs = fig.add_gridspec(2,2)   
+    gs = fig.add_gridspec(2, 2)   
+    symbol_map = dict(zip(policies, ['s', 'o', 'v', '*']))
 
-       
     def plot_performance(query_set: str, ax):
         dataset_df = performance_data[(performance_data['triplestore'] == triplestore) & (performance_data['dataset'] == dataset) & (performance_data['query_set'] == query_set)]
         means = dataset_df.groupby(['policy', 'snapshot']).mean()
         means = means.reset_index()
-            
-        color_map = dict(zip(policies, ['red', 'blue', 'green', 'purple']))
         
         for policy in policies:
             policy_df = means[means['policy'] == policy]
+            if dataset == 'bearc':
+                markevery = math.ceil(len(policy_df['snapshot'])/120)
+            else:
+                markevery = math.ceil(len(policy_df['snapshot'])/60)
             ax.set_yscale('log')
-            ax.plot(policy_df['snapshot'], policy_df['execution_time_total'], label=policy, color=color_map[policy])
+            ax.plot(policy_df['snapshot'], policy_df['execution_time_total'], linestyle='none',
+                marker=symbol_map[policy], markersize=7, markerfacecolor='none', markeredgewidth=1, drawstyle='default',
+                label=policy, color='black', markevery=markevery)
+
         
         ax.set_title(f"Query set: {query_set}")
         ax.set_xlabel('snapshots')
         ax.set_ylabel('Execution time (s)')
         tick_steps = max(math.floor(len(policy_df['snapshot'])/10), 1)
         ax.set_xticks(ticks=range(0, len(policy_df['snapshot']), tick_steps),
-                    labels=[*range(0, len(policy_df['snapshot']), tick_steps)])
+                      labels=[*range(0, len(policy_df['snapshot']), tick_steps)])
 
     query_sets = performance_data[performance_data['dataset'] == dataset]['query_set'].unique()
     if len(query_sets) == 1:
@@ -87,7 +91,6 @@ def create_plots(triplestore: str, dataset: str):
         ax2 = fig.add_subplot(gs[0, 1])
         plot_performance(query_set=query_sets[1], ax=ax2)
 
-
     def plot_ingestion(ax, ax2):
         bar_width = 0.2
         spacing = 0.1
@@ -99,7 +102,7 @@ def create_plots(triplestore: str, dataset: str):
             raw_size = policy_data["raw_file_size_MiB"].mean()
             db_size = policy_data["db_files_disk_usage_MiB"].mean()
             
-            ax.boxplot(policy_data["ingestion_time"], positions=[i], widths=0.8)
+            ax.boxplot(policy_data["ingestion_time"], positions=[i], widths=0.8, medianprops=dict(color='black', linestyle='--'))
 
             ax2.bar(i, raw_size, bar_width, alpha=opacity, color='limegreen', label="Raw File Size")
             ax2.bar(i + bar_width + spacing, db_size, bar_width, alpha=opacity, color='darkgreen', label="DB File Size")
@@ -108,10 +111,8 @@ def create_plots(triplestore: str, dataset: str):
     
         ax.set_xticks(index)
         ax.set_xticklabels(policies)
-        #ax.set_title("")
         ax.set_xlabel("Policies")
         ax.set_ylabel("Ingestion Time (s)")
-        ax.yaxis.label.set_color('coral')
 
         ax2.set_xticks([bar_width/2 + spacing/2 + x for x in range(len(policies))])
         ax2.set_xticklabels(policies)
@@ -128,30 +129,27 @@ def create_plots(triplestore: str, dataset: str):
     ###############################
 
     # Add legend
-    red_patch = mpatches.Patch(color='red', label='ic_sr_ng')
-    blue_patch = mpatches.Patch(color='blue', label='cb_sr_ng')
-    green_patch = mpatches.Patch(color='green', label='tb_sr_ng')
-    purple_patch = mpatches.Patch(color='purple', label='tb_sr_rs')
+    red_line = mlines.Line2D([], [], color='black', marker='s', linestyle='None', markersize=7, markeredgecolor='black', markerfacecolor='none', label='ic_sr_ng')
+    blue_line = mlines.Line2D([], [], color='black', marker='o', linestyle='None', markersize=7, markeredgecolor='black', markerfacecolor='none',label='cb_sr_ng')
+    green_line = mlines.Line2D([], [], color='black', marker='v', linestyle='None', markersize=7, markeredgecolor='black', markerfacecolor='none',label='tb_sr_ng')
+    purple_line = mlines.Line2D([], [], color='black', marker='*', linestyle='None', markersize=7, markeredgecolor='black', markerfacecolor='none',label='tb_sr_rs')
 
     limegreen_patch = mpatches.Patch(color='limegreen', label='Raw File Size')
     darkgreen_patch = mpatches.Patch(color='darkgreen', label='DB File Size')
-    coral_patch = mpatches.Patch(color='coral', label='Ingestion Time')
 
-    handles1 = [red_patch, blue_patch, green_patch, purple_patch]
-    handles2 = [coral_patch, limegreen_patch, darkgreen_patch]
-    fixed_labels = ['coral_patch', 'limegreen_patch', 'darkgreen_patch']
+    handles1 = [red_line, blue_line, green_line, purple_line]
+    handles2 = [limegreen_patch, darkgreen_patch]
+    fixed_labels = ['limegreen_patch', 'darkgreen_patch']
 
     fig.legend(loc="upper right", ncol=4, handles=sorted(handles1, key=lambda x: x.get_label()))
     fig.legend(loc="lower right", ncol=3, handles=sorted(handles2, key=lambda x: fixed_labels.index(x.get_label()) if x.get_label() in fixed_labels else len(fixed_labels)))
 
-    #fig.suptitle(f"Query performance and data ingestion & storage plots \nfor {triplestore} and {dataset}", fontsize=24)
     fig.set_figheight(9)
     fig.set_figwidth(16)
 
     plt.tight_layout(pad=3.0, w_pad=2, h_pad=1.0)
     plt.savefig(f"/starvers_eval/output/figures/time_{triplestore}_{dataset}.png")
     plt.close()
-
 
 def create_plots_update(triplestore: str, dataset: str):
     # Data
@@ -167,6 +165,8 @@ def create_plots_update(triplestore: str, dataset: str):
         data = performance_update_data[(performance_update_data['triplestore'] == triplestore) & (performance_update_data['dataset'] == dataset)]
 
         chunk_sizes = data['chunk_size'].unique().to_list()
+        line_styles = ['-', '--', '-.', ':']
+        
         for i, chunk_size in enumerate(chunk_sizes):
             data_chunk_size = data[data['chunk_size']==chunk_size]
             data_add = data_chunk_size.query('batch.str.startswith("snapshot") | batch.str.startswith("positive")')
@@ -174,44 +174,39 @@ def create_plots_update(triplestore: str, dataset: str):
             labels_add = data_add['batch'].str.split("_").str[-1].astype(str) + "\n" + np.floor(data_add['cnt_batch_trpls']/1000).astype(int).astype(str) + "k"
             labels_delete = data_delete['batch'].str.split("_").str[-1].astype(str) + "\n" + np.floor(data_delete['cnt_batch_trpls']/1000).astype(int).astype(str) + "k"
             
-            ax1.set_xticks([i for i in range(len(data_add))])
+            ax1.plot(data_add['batch'], data_add['execution_time'], label=f'Add {chunk_size}', linestyle=line_styles[i], color='blue')
+            ax1.set_xticks(range(len(labels_add)))
             ax1.set_xticklabels(labels_add)
-            ax2.set_xticks([i for i in range(len(data_delete))])
+            ax1.set_title('Add Performance')
+            ax1.set_xlabel('Batches')
+            ax1.set_ylabel('Execution Time (s)')
+            
+            ax2.plot(data_delete['batch'], data_delete['execution_time'], label=f'Delete {chunk_size}', linestyle=line_styles[i], color='blue')
+            ax2.set_xticks(range(len(labels_delete)))
             ax2.set_xticklabels(labels_delete)
-            ax1.plot(labels_add, data_add['execution_time'], label=str(chunk_size)) # label=str(chunk_size)
-            ax2.plot(labels_delete, data_delete['execution_time'], label=str(chunk_size))
+            ax2.set_title('Delete Performance')
+            ax2.set_xlabel('Batches')
+            ax2.set_ylabel('Execution Time (s)')
 
-    ax1 = fig.add_subplot(gs[0,0])
-    ax2 = fig.add_subplot(gs[1,0])
-    plot_performance_update(ax1, ax2)
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax2 = fig.add_subplot(gs[1, 0])
+    plot_performance_update(ax1=ax1, ax2=ax2)
 
     ###############################
     # Plot tuning and export
     ###############################
 
-    ax1.set_ylabel('Execution Time in s')
-    ax1.set_xlabel('Batch number and number of triples in batch')
-    ax1.set_title('Insert')
-    ax1.legend(loc='upper right')
-
-    ax2.set_ylabel('Execution Time in s')
-    ax2.set_xlabel('Batch number and number of triples in batch')
-    ax2.set_title('Invalidate')
-    ax2.legend(loc='upper right')
-
+    fig.legend(loc="upper right", ncol=4)
     fig.set_figheight(9)
     fig.set_figwidth(16)
-    fig.suptitle(f'Insert and Invalidate performance for a range of chunk sizes (1000-8000) for the {dataset} dataset and {triplestore}')
-    
+
     plt.tight_layout(pad=3.0, w_pad=2, h_pad=1.0)
     plt.savefig(f"/starvers_eval/output/figures/time_update_{triplestore}_{dataset}.png")
     plt.close()
-
 
 # Plots for query performance and ingestion
 args = itertools.product(['graphdb', 'jenatdb2'], datasets)
 list(map(lambda x: create_plots(*x), args))
 
 # Plots for update performance 
-create_plots_update("GRAPHDB", 'bearc')
-
+create_plots_update("graphdb", 'bearc')
