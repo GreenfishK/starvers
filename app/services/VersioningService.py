b36aa920292c609dd2026b368937e34adcb8f4a4
Martin Mitteregger <martin.mitteregger@datasys.at>
2025-04-09 00:05:40 +0200

    Fix issues after renaming fields

--- app/services/VersioningService.py
+++ app/services/VersioningService.py
@@ -37,24 +37,24 @@ class StarVersService(VersioningService):
     def __init__(self, tracking_task: TrackingTaskDto) -> None:
         self.tracking_task = tracking_task
 
-        self.__graph_db_get_endpoint = Settings().graph_db_url_get_endpoint.replace('{:repo_name}', tracking_task.repository_name)
-        self.__graph_db_post_endpoint = Settings().graph_db_url_post_endpoint.replace('{:repo_name}', tracking_task.repository_name)
+        self.__graph_db_get_endpoint = Settings().graph_db_url_get_endpoint.replace('{:repo_name}', tracking_task.name)
+        self.__graph_db_post_endpoint = Settings().graph_db_url_post_endpoint.replace('{:repo_name}', tracking_task.name)
         self.__starvers_engine = TripleStoreEngine(self.__graph_db_get_endpoint, self.__graph_db_post_endpoint, skip_connection_test=True)
         
         match tracking_task.delta_type:
             case DeltaType.ITERATIVE:
-                self.__delta_query_service = IterativeDeltaQueryService(self.__starvers_engine, tracking_task.rdf_dataset_url, tracking_task.repository_name_temp())
+                self.__delta_query_service = IterativeDeltaQueryService(self.__starvers_engine, tracking_task.rdf_dataset_url, tracking_task.name_temp())
             case DeltaType.SPARQL:
-                self.__delta_query_service = SparqlDeltaQueryService(self.__starvers_engine, tracking_task.rdf_dataset_url, tracking_task.repository_name_temp())
+                self.__delta_query_service = SparqlDeltaQueryService(self.__starvers_engine, tracking_task.rdf_dataset_url, tracking_task.name_temp())
 
 
     def run_initial_versioning(self, version_timestamp):
-        self.__delta_query_service.load_rdf_data(self.tracking_task.repository_name)
+        self.__delta_query_service.load_rdf_data(self.tracking_task.name)
         self.__starvers_engine.version_all_triples(initial_timestamp=version_timestamp)
 
 
     def query(self, query: str, timestamp: datetime = None, query_as_timestamped: bool = True):
-        LOG.info(f"Query at timestamp={timestamp} from repository {self.tracking_task.repository_name} with uuid={self.tracking_task.dataset_id}")
+        LOG.info(f"Query at timestamp={timestamp} from repository {self.tracking_task.name} with uuid={self.tracking_task.id}")
         return self.__starvers_engine.query(query, timestamp, query_as_timestamped)
     
 
@@ -76,7 +76,7 @@ class StarVersService(VersioningService):
             insertions, deletions = self.__delta_query_service.calculate_delta()  
             timing_delta = time.time_ns() - timing_delta
 
-            LOG.info(f"Found {len(insertions.index)} insertions and {len(deletions.index)} deletions for dataset with uuid={self.tracking_task.dataset_id}")
+            LOG.info(f"Found {len(insertions.index)} insertions and {len(deletions.index)} deletions for dataset with uuid={self.tracking_task.id}")
             
             timing_versioning = time.time_ns()
             insertions_n3 = convert_df_to_n3(insertions)
@@ -89,17 +89,17 @@ class StarVersService(VersioningService):
             timing_overall = time.time_ns() - timing_overall
 
             if (Settings().evaluation_mode):
-                path = f"./evaluation/{self.tracking_task.repository_name}/"
+                path = f"./evaluation/{self.tracking_task.name}/"
                 os.makedirs(os.path.dirname(path), exist_ok=True)
 
                 # Persist Timings
-                with open(f"{path}{self.tracking_task.repository_name}_timings.csv", "a+") as timing_file:
+                with open(f"{path}{self.tracking_task.name}_timings.csv", "a+") as timing_file:
                     timing_file.write(f"{get_timestamp(version_timestamp)}, {len(insertions_n3)}, {len(deletions_n3)}, {timing_prepare}, {timing_delta}, {timing_versioning}, {timing_overall}")
                     timing_file.write('\n')
                 
                 if len(insertions_n3) > 0 or len(deletions_n3) > 0:
                     # Persist Inserts, Deletions
-                    with open(f"{path}{get_timestamp(version_timestamp)}/{self.tracking_task.repository_name}_{get_timestamp(version_timestamp)}.delta", "a+") as dump_file:
+                    with open(f"{path}{get_timestamp(version_timestamp)}/{self.tracking_task.name}_{get_timestamp(version_timestamp)}.delta", "a+") as dump_file:
                         dump_file.writelines(map(lambda x: "- " + x + '\n', deletions_n3))
                         dump_file.writelines(map(lambda x: "+ " + x + '\n', insertions_n3))
                 shutil.make_archive(f"{path}{get_timestamp(version_timestamp)}.zip", "zip", f"{path}{get_timestamp(version_timestamp)}")
