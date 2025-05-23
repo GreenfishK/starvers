a345e80609f7678ffb1296166333e697ab8826b6
Filip Kovacevic <greenfish427@gmail.com>
2025-05-23 19:26:12 +0200

    add plots

--- app/gui/run.py
+++ app/gui/run.py
@@ -20,5 +20,7 @@ if __name__ == "__main__":
 # - A plot that shows the number of added and deleted triples over time for the selected repository. 
 # The input files can be found in /starvers/evaluation/{repo_name}/{repo_name}_timing.csv
 # The timestamp is in the first column, the number of added triples in the second column and the number of deleted triples in the third column.
-# - Range of accessible timestamps for the selected repository. Display the range in the following format: from: start_timestamp to: end_timestamp
+# The plot should be added under the Dropdown menu for the repo selection.
+# - Range of accessible timestamps for the selected repository. Display the range above the timestamp field as an info in the following format: from: start_timestamp to: end_timestamp
 # Both can be extracted from the first column in /starvers/evaluation/{repo_name}/{repo_name}_timing.csv
+# - Make the timestamp field smaller. Big enough to fit the timestamp, but not too big.
