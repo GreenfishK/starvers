399de518054b6a4adb8e0b449673ca00423dbcf2
Filip Kovacevic <greenfish427@gmail.com>
2025-08-15 21:19:47 +0200

    fix bugs with tests 1-3

--- tests/TestCompute.py
+++ tests/TestCompute.py
@@ -5,8 +5,14 @@
 # Passed
 # Manually executed on 15.08.2025 20:45
 
-# test 2: entrypoint: ["python", "app/utils/Compute.py", "air_quality_ontology_iterative", "ITERATIVE",  "from_scratch", "20250523-174303_935",
-# test 3: entrypoint: ["python", "app/utils/Compute.py", "air_quality_ontology_iterative", "ITERATIVE",  "from_version", "20250523-174303_935", -> should fail (In the 'from_version' versioning mode, the timestamp needs to be a snaps ...)
+# test 2: entrypoint: ["python", "app/utils/Compute.py", "air_quality_ontology_iterative", "ITERATIVE",  "from_scratch", "20250523-174303_935"]
+# Passed.
+# Manually executed on 15.08.2025 20:57
+
+# test 3: entrypoint: ["python", "app/utils/Compute.py", "air_quality_ontology_iterative", "ITERATIVE",  "from_version", "20250523-174303_935"] -> should fail (In the 'from_version' versioning mode, the timestamp needs to be a snaps ...)
+# Passed. 
+# Manually executed on 15.08.2025 21:19
+
 # test 4: entrypoint: ["python", "app/utils/Compute.py", "air_quality_ontology_iterative", "ITERATIVE",  "from_version", "20250523-174303_35", -> should fail (Invalid timestamp format ...)
 # test 5 (dependent on test 2 execution): entrypoint: ["python", "app/utils/Compute.py", "air_quality_ontology_iterative", "ITERATIVE",  "from_version", "20250815-162537_245", -> should work, if 20250815-162537_245.zip was not already processed. Make sure this version is not there when executing test 2
 # test 6: entrypoint: ["python", "app/utils/Compute.py", "air_quality_ontology_iterative", "ITERATIVE",  "from_version", -> should fail (Versioning mode 'from_version' requires ...)
