3913e7784ebac0774ad689ff2fbd876423ff1c45
Filip Kovacevic <greenfish427@gmail.com>
2025-08-25 18:16:49 +0200

    implement custom property identifiers

--- app/utils/graphdb/GraphDatabaseUtils.py
+++ app/utils/graphdb/GraphDatabaseUtils.py
@@ -15,6 +15,7 @@ from app.utils.exceptions.ServerFileImportFailedException import ServerFileImpor
 
 DEFAULT_GRAPH_NAME = 'http://rdf4j.org/schema/rdf4j#nil'
 BASE_GRAPH_URI = 'http://example.org/'
+query_dir = 'app/utils/graphdb/queries'
 
 def create_engine(repository_name: str, auth = DIGEST, method = POST, return_format = CSV, query_type = 'SELECT'):
     graph_db_get_endpoint = Settings().graph_db_url_get_endpoint.replace('{:repo_name}', repository_name)
@@ -28,7 +29,7 @@ def create_engine(repository_name: str, auth = DIGEST, method = POST, return_for
     return sparql_engine
 
 # Implementation for Graph DB
-def create_repository(repository_name: str): # add URL and description?
+def create_repository(repository_name: str): 
     repoConfig = __load_repo_config_file()
     repoConfig = repoConfig.replace('{:name}', repository_name)
     repoConfig = repoConfig.replace('{:description}', "Repository for versioned " + repository_name)
@@ -134,30 +135,30 @@ def delete_serverfile(file_name: str, repository_name: str):
 
 @lru_cache
 def __load_repo_config_file() -> str:
-    with open('app/utils/graphdb/repo-config.ttl', 'r') as f:
+    with open(f'app/utils/graphdb/repo-config.ttl', 'r') as f:
         return f.read()
     
 def get_query_all_template(graph_name: str = "") -> str:
-    with open('app/utils/graphdb/query_all_from_graph.sparql', 'r') as f:
+    with open(f'{query_dir}/query_all_from_graph.sparql', 'r') as f:
         template = f.read()
         template = template.replace('{:graph}', (BASE_GRAPH_URI + graph_name) if graph_name else DEFAULT_GRAPH_NAME )
         return template
 
 def get_construct_all_versioned_template(timestamp: datetime.datetime, graph_name: str = "") -> str:
-    with open('app/utils/graphdb/construct_all_from_versioned_graph.sparql', 'r') as f:
+    with open(f'{query_dir}/construct_all_from_versioned_graph.sparql', 'r') as f:
         template = f.read()
         template = template.replace('{:timestamp}', _versioning_timestamp_format(timestamp))
         template = template.replace('{:graph}', (BASE_GRAPH_URI + graph_name) if graph_name else DEFAULT_GRAPH_NAME )
         return template       
 
 def get_construct_all_template(graph_name: str = "") -> str:
-    with open('app/utils/graphdb/construct_all_from_graph.sparql', 'r') as f:
+    with open(f'{query_dir}/construct_all_from_graph.sparql', 'r') as f:
         template = f.read()
         template = template.replace('{:graph}', (BASE_GRAPH_URI + graph_name) if graph_name else DEFAULT_GRAPH_NAME )
         return template
     
 def get_count_triples_template(timestamp: datetime.datetime, graph_name: str = "") -> str:
-    with open('app/utils/graphdb/count_triples.sparql', 'r') as f:
+    with open(f'{query_dir}/count_triples.sparql', 'r') as f:
         template = f.read()
         template = template.replace('{:timestamp}', _versioning_timestamp_format(timestamp))
         template = template.replace('{:graph}', (BASE_GRAPH_URI + graph_name) if graph_name else DEFAULT_GRAPH_NAME )
@@ -165,55 +166,56 @@ def get_count_triples_template(timestamp: datetime.datetime, graph_name: str = "
     
 @lru_cache
 def get_drop_graph_template(graph_name: str) -> str:
-    with open('app/utils/graphdb/drop_graph.sparql', 'r') as f:
+    with open(f'{query_dir}/drop_graph.sparql', 'r') as f:
         template = f.read()
         template = template.replace('{:graph}', BASE_GRAPH_URI + graph_name)
         return template
     
 def get_delta_query_deletions_template(timestamp: datetime.datetime, graph_name: str) -> str:
-    with open('app/utils/graphdb/delta_query_deletions.sparql', 'r') as f:
+    with open(f'{query_dir}/delta_query_deletions.sparql', 'r') as f:
         template = f.read()
         template = template.replace('{:timestamp}', _versioning_timestamp_format(timestamp))
         template = template.replace('{:graph}', BASE_GRAPH_URI + graph_name)
         return template
     
 def get_delta_query_insertions_template(timestamp: datetime.datetime, graph_name: str) -> str:
-    with open('app/utils/graphdb/delta_query_insertions.sparql', 'r') as f:
+    with open(f'{query_dir}/delta_query_insertions.sparql', 'r') as f:
         template = f.read()
         template = template.replace('{:timestamp}', _versioning_timestamp_format(timestamp))
         template = template.replace('{:graph}', BASE_GRAPH_URI + graph_name)
         return template
 
 def get_all_creation_timestamps() -> str:
-    with open('app/utils/graphdb/query_creation_timestamps.sparql', 'r') as f:
+    with open(f'{query_dir}/query_creation_timestamps.sparql', 'r') as f:
         template = f.read()
         return template
 
 # Metric
 def get_snapshot_classes_template(ts_current: datetime.datetime, ts_prev: datetime.datetime) -> str:
-    with open('app/utils/graphdb/query_snapshot_classes.sparql', 'r') as f:
+    with open(f'{query_dir}/query_snapshot_classes.sparql', 'r') as f:
         template = f.read()
         template = template.replace('{:ts_current}', _versioning_timestamp_format(ts_current))
         template = template.replace('{:ts_prev}', _versioning_timestamp_format(ts_prev))
         return template
         
 # Metric
-def get_snapshot_properties_template(ts_current: datetime.datetime, ts_prev: datetime.datetime) -> str:
-    with open('app/utils/graphdb/query_snapshot_properties.sparql', 'r') as f:
+def get_snapshot_properties_template(ts_current: datetime.datetime, ts_prev: datetime.datetime, property_identifiers: str) -> str:
+    with open(f'{query_dir}/query_snapshot_properties.sparql', 'r') as f:
         template = f.read()
         template = template.replace('{:ts_current}', _versioning_timestamp_format(ts_current))
         template = template.replace('{:ts_prev}', _versioning_timestamp_format(ts_prev))
+        template = template.replace('{:property_identifiers}', property_identifiers)
         return template
 
 # Metric
 def get_dataset_static_core_template() -> str:
-    with open('app/utils/graphdb/query_static_core_triples.sparql', 'r') as f:
+    with open(f'{query_dir}/query_static_core_triples.sparql', 'r') as f:
         template = f.read()
         return template
 
 # Metric
 def get_dataset_version_oblivious_template() -> str:
-    with open('app/utils/graphdb/query_version_oblivious_triples.sparql', 'r') as f:
+    with open(f'{query_dir}/query_version_oblivious_triples.sparql', 'r') as f:
         template = f.read()
         return template
 
