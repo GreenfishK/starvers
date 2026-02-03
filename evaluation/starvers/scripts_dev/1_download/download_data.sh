b7a522bba93bb6a90839ecbaccac0d6231bba7b2
Filip Kovacevic <greenfish427@gmail.com>
2026-02-03 10:50:43 +0100

    update quereis and dataset infos

--- evaluation/starvers/scripts_dev/1_download/download_data.sh
+++ evaluation/starvers/scripts_dev/1_download/download_data.sh
@@ -18,42 +18,42 @@ datasets=("${datasets}")
 registered_datasets=$(grep -E '\[datasets\.[A-Za-z_]+\]' /starvers_eval/configs/eval_setup.toml | awk -F "." '{print $2}' | sed 's/.$//')
 echo "$(log_timestamp) ${log_level}: Registered datasets are ${registered_datasets} ..." >> $log_file
 
-for dataset in ${datasets[@]}; do
+#for dataset in ${datasets[@]}; do
 
-    if ! [[ ${registered_datasets[@]} =~ $dataset ]]; then
-        echo "$(log_timestamp) ${log_level}: Dataset ${dataset} is not within the registered datasets: ${registered_datasets} ..." >> $log_file
-        continue
-    fi
+#    if ! [[ ${registered_datasets[@]} =~ $dataset ]]; then
+#        echo "$(log_timestamp) ${log_level}: Dataset ${dataset} is not within the registered datasets: ${registered_datasets} ..." >> $log_file
+#        continue
+#    fi
 
-    download_link_snapshots=`grep -A 8 -E "\[datasets\.${dataset}\]" /starvers_eval/configs/eval_setup.toml | grep 'download_link_snapshots' | awk '{print $3}' | sed 's/"//g'`
-    archive_name_snapshots=`grep -A 8 -E "\[datasets\.${dataset}\]" /starvers_eval/configs/eval_setup.toml | grep 'archive_name_snapshots' | awk '{print $3}' | sed 's/"//g'`
-    download_link_ng_dataset=`grep -A 8 -E "\[datasets\.${dataset}\]" /starvers_eval/configs/eval_setup.toml | grep 'download_link_ng_dataset' | awk '{print $3}' | sed 's/"//g'`
-    archive_name_ng_dataset=`grep -A 8 -E "\[datasets\.${dataset}\]" /starvers_eval/configs/eval_setup.toml | grep 'archive_name_ng_dataset' | awk '{print $3}' | sed 's/"//g'`
-    yn_nested_archives=`grep -A 8 -E "\[datasets\.${dataset}\]" /starvers_eval/configs/eval_setup.toml | grep 'yn_nested_archives' | awk '{print $3}' | sed 's/"//g'`
+#    download_link_snapshots=`grep -A 8 -E "\[datasets\.${dataset}\]" /starvers_eval/configs/eval_setup.toml | grep 'download_link_snapshots' | awk '{print $3}' | sed 's/"//g'`
+#    archive_name_snapshots=`grep -A 8 -E "\[datasets\.${dataset}\]" /starvers_eval/configs/eval_setup.toml | grep 'archive_name_snapshots' | awk '{print $3}' | sed 's/"//g'`
+#    download_link_ng_dataset=`grep -A 8 -E "\[datasets\.${dataset}\]" /starvers_eval/configs/eval_setup.toml | grep 'download_link_ng_dataset' | awk '{print $3}' | sed 's/"//g'`
+#    archive_name_ng_dataset=`grep -A 8 -E "\[datasets\.${dataset}\]" /starvers_eval/configs/eval_setup.toml | grep 'archive_name_ng_dataset' | awk '{print $3}' | sed 's/"//g'`
+#    yn_nested_archives=`grep -A 8 -E "\[datasets\.${dataset}\]" /starvers_eval/configs/eval_setup.toml | grep 'yn_nested_archives' | awk '{print $3}' | sed 's/"//g'`
 
-    echo "$(log_timestamp) ${log_level}: Downloading ${dataset} snapshots..." >> $log_file
-    wget -t 3 -c -P /starvers_eval/rawdata/${dataset} ${download_link_snapshots}
-    mkdir -p /starvers_eval/rawdata/${dataset}/${snapshot_dir}
-    echo "$(log_timestamp) ${log_level}: Extracting ${dataset} snapshots..." >> $log_file
-    tar -xf /starvers_eval/rawdata/${dataset}/${archive_name_snapshots} -C /starvers_eval/rawdata/${dataset}/${snapshot_dir}
+#    echo "$(log_timestamp) ${log_level}: Downloading ${dataset} snapshots..." >> $log_file
+#    wget -t 3 -c -P /starvers_eval/rawdata/${dataset} ${download_link_snapshots}
+#    mkdir -p /starvers_eval/rawdata/${dataset}/${snapshot_dir}
+#    echo "$(log_timestamp) ${log_level}: Extracting ${dataset} snapshots..." >> $log_file
+#    tar -xf /starvers_eval/rawdata/${dataset}/${archive_name_snapshots} -C /starvers_eval/rawdata/${dataset}/${snapshot_dir}
     
-    if [[ $yn_nested_archives == "true" ]]; then
-        cd /starvers_eval/rawdata/${dataset}/${snapshot_dir}
-        for f in *.gz ; do gzip -d < "$f" > /starvers_eval/rawdata/${dataset}/${snapshot_dir}/"${f%.*}" ; done
-        rm *.gz
-    fi
+#    if [[ $yn_nested_archives == "true" ]]; then
+#        cd /starvers_eval/rawdata/${dataset}/${snapshot_dir}
+#        for f in *.gz ; do gzip -d < "$f" > /starvers_eval/rawdata/${dataset}/${snapshot_dir}/"${f%.*}" ; done
+#        rm *.gz
+#    fi
     
-    echo "$(log_timestamp) ${log_level}: Downloading ${dataset} named graphs dataset..." >> $log_file
-    wget -t 3 -c -P /starvers_eval/rawdata/${dataset} ${download_link_ng_dataset}
-    echo "$(log_timestamp) ${log_level}: Extracting ${dataset} named graphs dataset..." >> $log_file
-    gzip -d < /starvers_eval/rawdata/${dataset}/${archive_name_ng_dataset} > /starvers_eval/rawdata/${dataset}/alldata.TB.nq
+#    echo "$(log_timestamp) ${log_level}: Downloading ${dataset} named graphs dataset..." >> $log_file
+#    wget -t 3 -c -P /starvers_eval/rawdata/${dataset} ${download_link_ng_dataset}
+#    echo "$(log_timestamp) ${log_level}: Extracting ${dataset} named graphs dataset..." >> $log_file
+#    gzip -d < /starvers_eval/rawdata/${dataset}/${archive_name_ng_dataset} > /starvers_eval/rawdata/${dataset}/alldata.TB.nq
     
     # for CB and CBNG policy: empty initial delete changeset
-    > /starvers_eval/rawdata/${dataset}/empty.nt 
+#    > /starvers_eval/rawdata/${dataset}/empty.nt 
     
-    echo "$(log_timestamp) ${log_level}: Downloading and extracting ${dataset} datasets finished." >> $log_file
+#    echo "$(log_timestamp) ${log_level}: Downloading and extracting ${dataset} datasets finished." >> $log_file
 
-done
+#done
 
 # Download SqiQA-dataset.zip and extract queries
 wget -t 3 -c \
