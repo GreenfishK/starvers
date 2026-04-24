9757e893973710a42a0d447cac8a09fc5a716e5c
Filip Kovacevic <greenfish427@gmail.com>
2026-04-24 15:16:38 +0200

    fix bug with ostrich ingest

--- evaluation/starvers/scripts_dev/triple_store_mgmt/ostrich_aggchange_mgmt.sh
+++ evaluation/starvers/scripts_dev/triple_store_mgmt/ostrich_aggchange_mgmt.sh
@@ -136,10 +136,11 @@ EOF
 
     # H&P fork CLI: ostrich-evaluate ingest <strategy> <param> <patches_dir> <start> <end>
     cd "${database_dir}" \
-    && /opt/ostrich-hp/ostrich-evaluate ingest \
-            aggchange 2.0 \
-            "${dataset_dir_or_file}" \
-            1 "${versions}"
+        && ulimit -n 1048576 \
+        && /opt/ostrich-hp/ostrich-evaluate ingest \
+               aggchange 2.0 \
+               "${dataset_dir_or_file}" \
+               1 "${versions}"
 }
 
 ingest_empty() {
